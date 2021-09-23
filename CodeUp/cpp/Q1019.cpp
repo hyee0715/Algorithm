20cac3092a797da57b60bf432349a9dcb4fcb25d
hyee0715 <hyee0715@naver.com>
2021-09-23 17:52:23 +0900

    기초 100제

---
+++ CodeUp/cpp/Q1019.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int y, m, d;
+	cin >> y;
+	cin.ignore(256, '.');
+	cin >> m;
+	cin.ignore(256, '.');
+	cin >> d;
+
+	cout.width(4); cout.fill('0');
+	cout << y << ".";
+
+	cout.width(2); cout.fill('0');
+	cout << m << ".";
+
+	cout.width(2); cout.fill('0');
+	cout << d << endl;
+
+	return 0;
+
+}
