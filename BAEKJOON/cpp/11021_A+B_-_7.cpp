c39f8d18d1fb624d4c10b332ca3f6ce2e34288e2
hyee0715 <hyee0715@naver.com>
2022-03-25 19:17:48 +0900

    220325 upload

---
+++ BAEKJOON/cpp/11021_A+B_-_7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int a, b;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 1; i <= tc; i++) {
+		cin >> a >> b;
+		
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+
+	return 0;
+}
