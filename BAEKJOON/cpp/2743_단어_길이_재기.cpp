8e81ec899a7b208bb8b57f4202f5a2bcb2e4421e
hyee0715 <hyee0715@naver.com>
2022-03-26 13:46:10 +0900

    220326 upload

---
+++ BAEKJOON/cpp/2743_단어_길이_재기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	cout << s.size();
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
