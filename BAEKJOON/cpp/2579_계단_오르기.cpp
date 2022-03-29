6067d1df6ad0e4579d10be3754c07b0cfde5e616
hyee0715 <hyee0715@naver.com>
2022-03-29 17:07:00 +0900

    220329 upload

---
+++ BAEKJOON/cpp/2579_계단_오르기.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define MAX 301
+using namespace std;
+
+int n;
+int arr[MAX], dp[MAX];
+
+void input() {
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+}
+
+//dp[a] = b -> a번째 계단까지 왔을 때, a번재 계단을 밟았을 경우 얻을 수 있는 총 점수의 최댓값은 b이다.
+void solution() {
+	dp[0] = 0;
+	dp[1] = arr[1];
+	
+	for (int i = 2; i <= n; i++) {
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+	}
+
+	cout << dp[n];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
