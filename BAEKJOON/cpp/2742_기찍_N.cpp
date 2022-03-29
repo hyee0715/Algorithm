f2196aa7b1c04194c1b04a94802ded1017487184
hyee0715 <hyee0715@naver.com>
2022-03-29 12:46:36 +0900

    220329 upload

---
+++ BAEKJOON/cpp/2742_기찍_N.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+void input() {
+	cin >> n;
+}
+
+void solution() {
+	for (int i = n; i >= 1; i--) {
+		cout << i << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
