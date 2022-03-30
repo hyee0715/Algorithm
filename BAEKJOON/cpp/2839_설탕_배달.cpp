ff1764b1fada9ad62bece8353e5f092b8f00086d
hyee0715 <hyee0715@naver.com>
2022-03-30 15:51:59 +0900

    220330 upload

---
+++ BAEKJOON/cpp/2839_설탕_배달.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define INF 987654321
+using namespace std;
+
+int n, n_copy, cnt;
+int answer = INF;
+
+void input() {
+	cin >> n;
+}
+
+void solution() {
+	if (n % 3 == 0) {
+		answer = n / 3;
+	}
+
+	for (int i = 1; ; i++) {
+		cnt = 0;
+
+		if (5 * i > n)
+			break;
+
+		n_copy = n - (5 * i);
+		cnt += i;
+
+		if (n_copy % 3 != 0)
+			continue;
+		else {
+			cnt += (n_copy / 3);
+			answer = min(answer, cnt);
+		}
+	}
+
+	if (answer == INF)
+		cout << "-1";
+	else
+		cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
