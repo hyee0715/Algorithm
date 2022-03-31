bf0b2d5c39b88be6751dde829e12c55c3b01dbe9
hyee0715 <hyee0715@naver.com>
2022-04-01 08:55:05 +0900

    220401 upload

---
+++ BAEKJOON/cpp/11722_가장_긴_감소하는_부분_수열.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define MAX 1001
+using namespace std;
+
+int n;
+int arr[MAX], dp[MAX];
+
+void input() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	int answer = 0;
+
+	for (int i = 1; i <= n; i++) {
+		dp[i] = 1;
+
+		for (int j = 1; j < i; j++) {
+			if (arr[i] < arr[j])
+				dp[i] = max(dp[j] + 1, dp[i]);
+		}
+
+		answer = max(answer, dp[i]);
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
