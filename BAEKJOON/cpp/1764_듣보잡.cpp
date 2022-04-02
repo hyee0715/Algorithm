4a8307a7af562adfb167bdd4e1006d47851386e4
hyee0715 <hyee0715@naver.com>
2022-04-02 09:18:32 +0900

    220402 upload

---
+++ BAEKJOON/cpp/1764_듣보잡.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+set<string> s;
+vector<string> v;
+
+void solution() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+
+		s.insert(str);
+	}
+
+	for (int i = 0; i < m; i++) {
+		string str;
+		cin >> str;
+
+		if (s.find(str) != s.end()) {
+			v.push_back(str);
+		}
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v.size() << "\n";
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
