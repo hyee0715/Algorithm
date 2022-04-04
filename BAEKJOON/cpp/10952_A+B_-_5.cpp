892bd7e95ad9767d8bd813abcbeb4588d0a78e12
hyee0715 <hyee0715@naver.com>
2022-04-04 15:55:07 +0900

    220404 upload

---
+++ BAEKJOON/cpp/10952_A+B_-_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int a, b;
+
+void solution() {
+	cout << a + b << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> a >> b;
+
+		if (a == 0 && b == 0)
+			break;
+		
+		solution();
+	}
+
+	return 0;
+}
