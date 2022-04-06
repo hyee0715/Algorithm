fc825a2c2fd68a8e247b3f44e4ed49414d1a124d
hyee0715 <hyee0715@naver.com>
2022-04-06 15:38:30 +0900

    220406 upload

---
+++ BAEKJOON/cpp/2588_곱셈.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int a;
+string b;
+
+void input() {
+	cin >> a >> b;
+}
+
+void solution() {
+	int n;
+
+	for (int i = 2; i >= 0; i--) {
+		n = b[i] - '0';
+
+		cout << a * n << "\n";
+	}
+	
+	
+	n = stoi(b);
+	cout << a * n;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
