74d570f53a04441856c002ae90a2ce0c1c98ca6f
hyee0715 <hyee0715@naver.com>
2022-04-10 06:14:08 +0900

    220410 upload

---
+++ BAEKJOON/cpp/22966_가장_쉬운_문제를_찾는_문제.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int n;
+vector<pair<int, string> > v;
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		int a;
+
+		cin >> s >> a;
+
+		v.push_back({ a, s });
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end());
+
+	cout << v[0].second;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
