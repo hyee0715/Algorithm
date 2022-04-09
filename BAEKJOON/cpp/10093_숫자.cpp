ad7dc834348650cdf1b4ddd26b89b5a6420939c3
hyee0715 <hyee0715@naver.com>
2022-04-09 16:10:14 +0900

    220409 upload

---
+++ BAEKJOON/cpp/10093_숫자.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long a, b;
+
+void input() {
+	cin >> a >> b;
+}
+
+void solution() {
+	if (a < b) {
+		cout << (b - a) - 1 << "\n";
+		for (long long i = a + 1; i < b; i++) {
+			cout << i << " ";
+		}
+	}
+	else {
+		cout << (a - b) - 1 << "\n";
+		for (long long i = b + 1; i < a; i++) {
+			cout << i << " ";
+		}
+	}
+	
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
