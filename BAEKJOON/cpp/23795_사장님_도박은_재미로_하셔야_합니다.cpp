662dd03c58dfc228493043a26d98988057b924ae
hyee0715 <hyee0715@naver.com>
2022-04-11 16:10:40 +0900

    220411 upload

---
+++ BAEKJOON/cpp/23795_사장님_도박은_재미로_하셔야_합니다.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, answer = 0;
+
+void solution() {
+	while (1) {
+		cin >> n;
+
+		if (n == -1)
+			break;
+
+		answer += n;
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
