8279f0e0c7bff54f0dfbc73ce0f92d5fc47cf806
hyee0715 <hyee0715@naver.com>
2022-04-12 09:23:41 +0900

    220412 upload

---
+++ BAEKJOON/cpp/10819_차이를_최대로.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+#define MAX 8
+using namespace std;
+
+int n, answer_max = 0;
+int arr[MAX];
+vector<int> v;
+bool visit[MAX];
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
+
+int find_sum() {
+	int sum = 0;
+
+	for (int i = 0; i < n - 1; i++) {
+		sum += (abs(v[i] - v[i + 1]));
+	}
+
+	return sum;
+}
+
+//순열
+void dfs(int cnt) {
+	if (cnt == n) {
+		answer_max = max(answer_max, find_sum());
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (visit[i] == true) continue;
+		visit[i] = true;
+		v.push_back(arr[i]);
+		dfs(cnt + 1);
+		v.pop_back();
+		visit[i] = false;
+	}
+}
+
+void solution() {
+	dfs(0);
+
+	cout << answer_max;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
