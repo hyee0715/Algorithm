38b5a1cadfdd207795290dc268701217a5e1a528
hyee0715 <hyee0715@naver.com>
2022-04-15 08:11:00 +0900

    220415 upload

---
+++ BAEKJOON/cpp/7510_고급_수학.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int tc, cnt = 1;
+long long a, b, c;
+
+void input() {
+	cin >> a >> b >> c;
+}
+
+bool find_right_triangle() {
+	if (pow(a, 2) == pow(b, 2) + pow(c, 2))
+		return true;
+
+	if (pow(b, 2) == pow(a, 2) + pow(c, 2))
+		return true;
+
+	if (pow(c, 2) == pow(a, 2) + pow(b, 2))
+		return true;
+
+	return false;
+}
+
+void solution() {
+	if (find_right_triangle())
+		cout << "Scenario #" << cnt << ":\n" << "yes\n";
+	else
+		cout << "Scenario #" << cnt << ":\n" << "no\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> tc;
+
+	while (cnt <= tc) {
+		input();
+		solution();
+
+		cout << "\n";
+		cnt++;
+	}
+	
+	return 0;
+}
