8c516a1d988ccfdce11660a2c369799b6e4d2458
hyee0715 <hyee0715@naver.com>
2022-04-14 16:20:31 +0900

    220414 upload

---
+++ BAEKJOON/cpp/5597_과제_안_내신_분.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstring>
+
+#define MAX 31
+using namespace std;
+
+int n;
+bool students[30];
+
+void input() {
+	memset(students, false, sizeof(students));
+
+	for (int i = 1; i <= 28; i++) {
+		cin >> n;
+		students[n] = true;
+	}
+}
+
+void solution() {
+	for (int i = 1; i <= 30; i++) {
+		if (students[i] == false) {
+			cout << i << "\n";
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+}
