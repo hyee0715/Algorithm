485f6c06889870cc22ab73c7471f5fdb0194a721
hyee0715 <hyee0715@naver.com>
2022-04-16 12:04:03 +0900

    220416 upload

---
+++ BAEKJOON/cpp/2386_도비의_영어_공부.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+char c;
+string s;
+vector<pair<char, int> > answer;
+
+void solution() {
+	while (1) {
+		cin >> c;
+
+		if (c == '#') break;
+
+		getline(cin, s);
+
+		int cnt = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (tolower(s[i]) == c)
+				cnt++;
+		}
+
+		answer.push_back({ c, cnt });
+	}
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i].first << " " << answer[i].second << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
