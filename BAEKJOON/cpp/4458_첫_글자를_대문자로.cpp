9f4b85a99699ed7b68be58c4d74f0299c6695528
hyee0715 <hyee0715@naver.com>
2022-04-17 16:25:10 +0900

    220417 upload

---
+++ BAEKJOON/cpp/4458_첫_글자를_대문자로.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int n;
+string s;
+vector<string> v;
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		getline(cin, s);
+		v.push_back(s);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < v.size(); i++) {
+		 toupper(v[i][0]);
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
