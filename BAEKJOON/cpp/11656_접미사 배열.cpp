eebe6dd424d05604964c9500c9938ff1f8f50086
hyee0715 <hyee0715@naver.com>
2022-04-18 09:21:10 +0900

    220418 upload

---
+++ BAEKJOON/cpp/11656_접미사 배열.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string s;
+vector<string> answer;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	string s_suffix;
+
+	int size = s.size();
+	for (int i = 0; i < size; i++) {
+		s_suffix = s.substr(i, size - i);
+
+		answer.push_back(s_suffix);
+	}
+
+	sort(answer.begin(), answer.end());
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
