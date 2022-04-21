4bdc8956bb25588c540d67781307b84b8261fad3
hyee0715 <hyee0715@naver.com>
2022-04-21 14:25:56 +0900

    220421 upload

---
+++ BAEKJOON/cpp/2495_연속구간.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int cnt, answer;
+string arr[3];
+
+void input() {
+	for (int i = 0; i < 3; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	for (int i = 0; i < 3; i++) {
+		cnt = 1;
+		answer = 1;
+
+		for (int j = 0; j < 8 - 1; j++) {
+			if (arr[i][j] == arr[i][j + 1]) {
+				cnt++;
+				answer = max(answer, cnt);
+			}
+			else {
+				cnt = 1;
+			}
+		}
+
+		cout << answer << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
