4bcc559be0dd662388c0f934d8c91aa90d663cff
hyee0715 <hyee0715@naver.com>
2022-04-22 09:32:36 +0900

    220422 upload

---
+++ BAEKJOON/cpp/2355_시그마.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+long long a, b;
+
+void input() {
+	cin >> a >> b;
+}
+
+void solution() {
+	if (a <= b) {
+		cout << (b - a + 1) * (b + a) / 2;
+	}
+	else { // a > b
+		cout << (a - b + 1) * (b + a) / 2;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
