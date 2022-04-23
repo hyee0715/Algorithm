2c88a1f2ba90ed2f9ea251b8c22c944a897f691f
hyee0715 <hyee0715@naver.com>
2022-04-23 14:21:54 +0900

    220423 upload

---
+++ BAEKJOON/cpp/11006_남욱이의_닭장.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m; // n : 모든 닭의 다리 수의 합, m : 닭의 수
+int u, t; // u : 다리가 잘린 닭의 수, t : 멀쩡한 닭의 수
+int original_leg_num; // 원래 있어야 할 다리 갯수
+
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	u = 0, t = 0;
+
+	original_leg_num = m * 2;
+
+	u = original_leg_num - n;
+
+	t = m - u;
+
+	cout << u << " " << t << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
