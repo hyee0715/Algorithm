37835581e90471708af6b79c5200eac9563c2c41
hyee0715 <hyee0715@naver.com>
2022-04-25 18:41:12 +0900

    220425 upload

---
+++ BAEKJOON/cpp/11727_2×n_타일링_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+#define MAX 1001
+using namespace std;
+
+int n;
+int dp[MAX];
+
+void input() {
+	cin >> n;
+}
+
+//dp[a] = b -> 2 * a 크기의 직사각형을 채우는 방법의 수는 b개이다.
+void solution() {
+	dp[1] = 1;
+	dp[2] = 3;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+	}
+
+	cout << dp[n];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
