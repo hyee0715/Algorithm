08402609cc7d93183a68caf0f003b1df4c008212
hyee0715 <hyee0715@naver.com>
2022-05-07 18:16:16 +0900

    220507 upload

---
+++ BAEKJOON/cpp/14681_사분면_고르기.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int x, y;
+
+void input() {
+	cin >> x >> y;
+}
+
+void solution() {
+	if (x > 0 && y > 0) {
+		cout << 1;
+	}
+	else if (x < 0 && y > 0) {
+		cout << 2;
+	}
+	else if (x < 0 && y < 0) {
+		cout << 3;
+	}
+	else {
+		cout << 4;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
