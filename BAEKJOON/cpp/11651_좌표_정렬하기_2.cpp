5355ecdbf456936926692f4c7dfc3fbff237b0a5
hyee0715 <hyee0715@naver.com>
2022-05-13 16:36:42 +0900

    220513 upload

---
+++ BAEKJOON/cpp/11651_좌표_정렬하기_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector<pair<int, int> > v;
+
+void input() {
+	cin >> n;
+
+	int a, b;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		v.push_back({ a, b });
+	}
+}
+
+bool compare(pair<int, int> p1, pair<int, int> p2) {
+	if (p1.second == p2.second) {
+		return p1.first < p2.first;
+	}
+	else {
+		return p1.second < p2.second;
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
