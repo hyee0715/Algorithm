c3ff42726985bd6917e3090f06a09de7ee1d572c
hyee0715 <hyee0715@naver.com>
2022-05-14 19:10:40 +0900

    220514 upload

---
+++ BAEKJOON/cpp/1259_팰린드롬수.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string s;
+
+void solution() {
+	string reverse_s = s;
+	reverse(reverse_s.begin(), reverse_s.end());
+
+	if (reverse_s == s)
+		cout << "yes\n";
+	else
+		cout << "no\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> s;
+
+		if (s == "0")
+			break;
+		
+		solution();
+	}
+
+	return 0;
+}
