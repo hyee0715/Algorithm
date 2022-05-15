bf029cda88b1d3809b2adecda85588579e81c13f
hyee0715 <hyee0715@naver.com>
2022-05-15 15:51:06 +0900

    220515 upload

---
+++ BAEKJOON/cpp/11047_동전_0.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, k, answer = 0;
+int arr[10];
+
+void input() {
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	//k원을 만드는 데 필요한 동전 개수의 최솟값 구하기
+	for (int i = n - 1; i >= 0; i--) {
+		answer += k / arr[i];
+		k = k % arr[i];
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
