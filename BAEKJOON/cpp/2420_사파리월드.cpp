90f0a6f715ac5e0373459daf76887cb5c2c3a5cf
hyee0715 <hyee0715@naver.com>
2022-05-26 13:25:51 +0900

    220526 upload

---
+++ BAEKJOON/cpp/2420_사파리월드.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long n, m;
+
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	cout << abs(n - m);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
