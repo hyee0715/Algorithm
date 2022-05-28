93baaa17e4b8b93ac595a9a0ec001012eb0db356
hyee0715 <hyee0715@naver.com>
2022-05-28 11:31:36 +0900

    220528 upload

---
+++ BAEKJOON/cpp/9713_Sum_of_Odd_Sequence.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int t, n, sum;
+
+void solution() {
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		sum = 0;
+		for (int j = 0; j <= n; j++) {
+			if (j % 2 == 1)
+				sum += j;
+		}
+	
+		cout << sum << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
