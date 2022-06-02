4dd5ee3b41c9d2e76de190fb23837a53ed6a20fc
hyee0715 <hyee0715@naver.com>
2022-06-02 15:13:18 +0900

    220602 upload

---
+++ BAEKJOON/cpp/10987_모음의_개수.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+int answer = 0;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			answer++;
+		}
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
