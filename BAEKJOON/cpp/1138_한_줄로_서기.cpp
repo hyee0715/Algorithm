3aa159cfe199c11c0a58d112fc5be0bd31fabfd8
hyee0715 <hyee0715@naver.com>
2022-06-03 14:15:28 +0900

    220603 upload

---
+++ BAEKJOON/cpp/1138_한_줄로_서기.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <vector>
+
+#define MAX 11
+using namespace std;
+
+int n, arr[MAX];
+bool visit[MAX];
+vector<int> v;
+
+void input() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+}
+
+bool check() {
+	for (int i = 0; i < v.size(); i++) {
+		int cnt = 0;
+		
+		for (int j = 0; j < i; j++) {
+			if (v[j] > v[i]) {
+				cnt++;
+			}
+		}
+
+		if (cnt != arr[v[i]])
+			return false;
+	}
+
+	return true;
+}
+
+void dfs(int cnt) {
+	if (cnt == n) {
+		if (check()) {
+			for (int i = 0; i < v.size(); i++) {
+				cout << v[i] << " ";
+			}
+		}
+
+		return;
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] == true) continue;
+			visit[i] = true;
+			v.push_back(i);
+			dfs(cnt + 1);
+			visit[i] = false;
+			v.pop_back();
+		}
+	}
+}
+
+void solution() {
+	dfs(0);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/551_Student_Attendance_Record_I.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+	bool checkRecord(string s) {
+		int A_cnt = 0, L_cnt = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'A') {
+				A_cnt++;
+			}
+
+			if (A_cnt >= 2) {
+				return false;
+			}
+
+			if (s[i] == 'L') {
+				L_cnt++;
+			}
+			else {
+				L_cnt = 0;
+			}
+
+			if (L_cnt > 2) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
