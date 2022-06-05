4b0a4c8e41cbd4eafa20bb8e34b3232ebac817cc
hyee0715 <hyee0715@naver.com>
2022-06-05 17:32:33 +0900

    220605 upload

---
+++ BAEKJOON/cpp/2075_N번째_큰_수.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+priority_queue<int, vector<int>, greater<int> > pq;
+
+void solution() {
+	cin >> n;
+
+	int num;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> num;
+
+			pq.push(num);
+
+			if (pq.size() > n)
+				pq.pop();
+		}
+	}
+
+	cout << pq.top();
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/164_Maximum_Gap.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	int maximumGap(vector<int>& nums) {
+		int max_gap = 0;
+
+		if (nums.size() < 2) {
+			return 0;
+		}
+
+		sort(nums.begin(), nums.end());
+
+		for (int i = 0; i < nums.size() - 1; i++) {
+			if (nums[i + 1] - nums[i] > max_gap) {
+				max_gap = nums[i + 1] - nums[i];
+			}
+		}
+
+		return max_gap;
+	}
+};
