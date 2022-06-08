f614a328103bc1d866759b2eda9689511b48ac03
hyee0715 <hyee0715@naver.com>
2022-06-08 16:04:34 +0900

    220608_upload

---
+++ BAEKJOON/cpp/2467_용액.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+
+#define INF 0x7FFFFFFF
+#define MAX 100001
+using namespace std;
+
+int n;
+long long arr[MAX];
+vector<long long> answer;
+
+void input() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	int start = 0;
+	int end = n - 1;
+	long long distance = INF;
+
+	while (start < end) {
+		if (distance > abs(arr[start] + arr[end])) {
+			distance = abs(arr[start] + arr[end]);
+
+			answer.clear();
+			answer.push_back(arr[start]);
+			answer.push_back(arr[end]);
+		}
+
+		if (arr[start] + arr[end] > 0)
+			end--;
+		else
+			start++;
+	}
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << " ";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/78_Subsets.cpp
@@ -0,0 +1,33 @@
+﻿class Solution {
+public:
+	bool visit[10];
+	vector<int> v;
+	vector<vector<int>> answer;
+
+	void dfs(int idx, int cnt, int limit, vector<int>& nums) {
+		if (cnt == limit) {
+			answer.push_back(v);
+		}
+		else {
+			for (int i = idx; i < nums.size(); i++) {
+				if (visit[i] == true) continue;
+				visit[i] = true;
+				v.push_back(nums[i]);
+				dfs(i, cnt + 1, limit, nums);
+				visit[i] = false;
+				v.pop_back();
+			}
+		}
+	}
+
+	vector<vector<int>> subsets(vector<int>& nums) {
+		answer.push_back(v);
+
+		for (int i = 1; i <= nums.size(); i++) {
+			memset(visit, false, sizeof(visit));
+			dfs(0, 0, i, nums);
+		}
+
+		return answer;
+	}
+};
