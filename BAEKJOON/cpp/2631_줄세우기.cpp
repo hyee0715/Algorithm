91f48a532a4c235c8ba0110d55f83df18d20c6f6
hyee0715 <hyee0715@naver.com>
2022-06-09 18:09:38 +0900

    220609 upload

---
+++ BAEKJOON/cpp/2631_줄세우기.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define MAX 201
+using namespace std;
+
+int n, max_num = 0;
+int arr[MAX], dp[MAX];
+
+void input() {
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	//LIS
+	for (int i = 1; i <= n; i++) {
+		dp[i] = 1;
+
+		for (int j = 1; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+
+		max_num = max(max_num, dp[i]);
+	}
+
+	cout << n - max_num;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/349_Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+	unordered_map<int, int> um1, um2;
+	vector<int> answer;
+
+public:
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+		for (int i = 0; i < nums1.size(); i++) {
+			if (um1.find(nums1[i]) == um1.end()) {
+				um1[nums1[i]] = 1;
+			}
+		}
+
+		for (int i = 0; i < nums2.size(); i++) {
+			if (um1.find(nums2[i]) != um1.end() && um2.find(nums2[i]) == um2.end()) {
+				um2[nums2[i]] = 1;
+				answer.push_back(nums2[i]);
+			}
+		}
+
+		return answer;
+	}
+};
