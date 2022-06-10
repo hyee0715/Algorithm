134cd432f0201d2081ad91a78731947525867764
hyee0715 <hyee0715@naver.com>
2022-06-10 17:35:54 +0900

    220610 upload

---
+++ BAEKJOON/cpp/5988_홀수일까_짝수일까.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string s;
+
+void solution() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+
+		if ((s[s.size() - 1] - '0') % 2 == 0)
+			cout << "even\n";
+		else
+			cout << "odd\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
