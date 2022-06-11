1ae868ad845700323aea4cb702b6a04675516d45
hyee0715 <hyee0715@naver.com>
2022-06-11 19:42:37 +0900

    220611 upload

---
+++ BAEKJOON/cpp/1449_수리공_항승.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define MAX 1010
+using namespace std;
+
+int n, l, cnt = 1;
+int arr[MAX];
+
+void input() {
+	cin >> n >> l;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
+
+void solution() {
+	//오름차순 정렬
+	sort(arr, arr + n);
+
+	int point = arr[0];
+
+	for (int i = 1; i < n; i++) {
+		if (arr[i] - point + 1 > l) {
+			cnt++;
+			point = arr[i];
+		}
+ 	}
+
+	cout << cnt;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/20_Valid_Parentheses.cpp
@@ -0,0 +1,55 @@
+﻿class Solution {
+	stack<char> st;
+public:
+	bool isValid(string s) {
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+				st.push(s[i]);
+			}
+			else if (s[i] == ')') {
+				if (st.empty()) {
+					return false;
+				}
+				else {
+					if (st.top() == '(') {
+						st.pop();
+					}
+					else {
+						return false;
+					}
+				}
+			}
+			else if (s[i] == ']') {
+				if (st.empty()) {
+					return false;
+				}
+				else {
+					if (st.top() == '[') {
+						st.pop();
+					}
+					else {
+						return false;
+					}
+				}
+			}
+			else if (s[i] == '}') {
+				if (st.empty()) {
+					return false;
+				}
+				else {
+					if (st.top() == '{') {
+						st.pop();
+					}
+					else {
+						return false;
+					}
+				}
+			}
+		}
+
+		if (!st.empty())
+			return false;
+
+		return true;
+	}
+};
