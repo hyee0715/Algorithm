f988d27901f3f928a4e0aa0923f0501fd3cb1b49
hyee0715 <hyee0715@naver.com>
2022-07-01 18:50:46 +0900

    220701 upload

---
+++ BAEKJOON/cpp/10817_세_수.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+
+void input() {
+	for (int i = 0; i < 3; i++) {
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end());
+
+	cout << v[1];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/186_Reverse_Words_in_a_String_II.cpp
@@ -0,0 +1,32 @@
+﻿class Solution {
+public:
+	void reverseWords(vector<char>& s) {
+		vector<string> v;
+		string str = "";
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] != ' ') {
+				str += s[i];
+			}
+			else {
+				v.push_back(str);
+				str = "";
+			}
+		}
+
+		v.push_back(str);
+		reverse(v.begin(), v.end());
+
+		s.clear();
+
+		for (int i = 0; i < v.size(); i++) {
+
+			for (int j = 0; j < v[i].size(); j++) {
+				s.push_back(v[i][j]);
+			}
+			s.push_back(' ');
+		}
+
+		s.pop_back();
+	}
+};
