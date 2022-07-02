4a360332381b083e35d1f674e34af799045d335b
hyee0715 <hyee0715@naver.com>
2022-07-02 17:58:39 +0900

    220702 upload

---
+++ BAEKJOON/cpp/2744_대소문자_바꾸기.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 65 && s[i] <= 90) { //대문자
+			s[i] = tolower(s[i]);
+		}
+		else { //소문자
+			s[i] = toupper(s[i]);
+		}
+	}
+
+	cout << s;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/41_First_Missing_Positive.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	int firstMissingPositive(vector<int>& nums) {
+		unordered_set<int> s;
+
+		for (int i = 0; i < nums.size(); i++) {
+			s.insert(nums[i]);
+		}
+
+		int cnt = 1;
+
+		while (1) {
+			if (s.find(cnt) == s.end()) {
+				break;
+			}
+
+			cnt++;
+		}
+
+		return cnt;
+	}
+};
