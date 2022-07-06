8473baeb4b7bbdbb87ce443a9410f648b46c9f64
hyee0715 <hyee0715@naver.com>
2022-07-06 18:35:04 +0900

    220706 upload

---
+++ BAEKJOON/cpp/1932_정수_삼각형.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <algorithm>
+
+#define MAX 500
+using namespace std;
+
+int n, answer = 0;
+int arr[MAX][MAX], dp[MAX][MAX];
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			cin >> arr[i][j];
+		}
+	}
+}
+
+void solution() {
+	dp[0][0] = arr[0][0];
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0)
+				dp[i][j] = dp[i - 1][j] + arr[i][j];
+			else if (i == j)
+				dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+			else
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+		answer = max(answer, dp[n - 1][i]);
+	
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/448_Find_All_Numbers_Disappeared_in_an_Array.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+	vector<int> findDisappearedNumbers(vector<int>& nums) {
+		vector<int> v(nums.size() + 1, 0), answer;
+
+		for (int i = 0; i < nums.size(); i++) {
+			v[nums[i]]++;
+		}
+
+		for (int i = 1; i < v.size(); i++) {
+			if (v[i] == 0)
+				answer.push_back(i);
+
+		}
+
+		return answer;
+	}
+};
