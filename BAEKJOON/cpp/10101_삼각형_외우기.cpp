958a889b9efe32b078d7a75516e08272034fbd7e
hyee0715 <hyee0715@naver.com>
2022-07-07 18:07:20 +0900

    220707 upload

---
+++ BAEKJOON/cpp/10101_삼각형_외우기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int a, b, c;
+
+void input() {
+	cin >> a >> b >> c;
+	
+}
+
+void solution() {
+	if (a + b + c != 180)
+		cout << "Error";
+	else if (a == b && b == c)
+		cout << "Equilateral";
+	else if (a == b || a == c || b == c)
+		cout << "Isosceles";
+	else 
+		cout << "Scalene";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
