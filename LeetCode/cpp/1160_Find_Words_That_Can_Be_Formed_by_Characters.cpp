309d82ab6f651fa850e74f34928f708bc6aabb22
hyee0715 <hyee0715@naver.com>
2022-07-09 14:46:51 +0900

    220709 upload

---
+++ LeetCode/cpp/1160_Find_Words_That_Can_Be_Formed_by_Characters.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+	int countCharacters(vector<string>& words, string chars) {
+		unordered_map<char, int> um, temp;
+		int answer = 0;
+		bool flag;
+
+		for (int i = 0; i < chars.size(); i++)
+			um[chars[i]]++;
+
+		for (int i = 0; i < words.size(); i++) {
+			temp = um;
+			flag = false;
+
+			for (int j = 0; j < words[i].size(); j++) {
+				temp[words[i][j]]--;
+
+				if (temp[words[i][j]] < 0) {
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag)
+				answer += words[i].size();
+		}
+
+		return answer;
+	}
+};
