7054ceedfed283691dd8144832f894b79c1cd996
hyee0715 <hyee0715@naver.com>
2022-07-10 18:46:16 +0900

    220710 upload

---
+++ BAEKJOON/cpp/23825_SASA_모형을_만들어보자.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	n = n / 2;
+	m = m / 2;
+
+	if (n < m)
+		cout << n;
+	else
+		cout << m;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/266_Palindrome_Permutation.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	bool canPermutePalindrome(string s) {
+		unordered_map<char, int> um;
+		int odd = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			um[s[i]]++;
+		}
+
+		for (auto pair : um) {
+			if (pair.second % 2 == 1) {
+				odd++;
+			}
+
+			if (odd > 1)
+				return false;
+		}
+
+		return true;
+	}
+};
