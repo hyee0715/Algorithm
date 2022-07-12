da947e4ada367aecc2c4bf8ec0cf11f93f3ead92
hyee0715 <hyee0715@naver.com>
2022-07-12 18:19:57 +0900

    220712 upload

---
+++ BAEKJOON/cpp/2675_문자열_반복.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string s;
+
+void input() {
+	cin >> n >> s;
+}
+
+void solution() {
+	for (int i = 0; i < s.size(); i++) {
+		for (int j = 0; j < n; j++) {
+			cout << s[i];
+		}
+	}
+
+	cout << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc; 
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
