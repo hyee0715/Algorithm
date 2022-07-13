6583e4811fc7788343017cea0021ba9da1d0ff9a
hyee0715 <hyee0715@naver.com>
2022-07-13 18:20:44 +0900

    220713 upload

---
+++ BAEKJOON/cpp/10102_개표.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, a = 0, b = 0;
+char c;
+
+void input() {
+	cin >> n;
+}
+
+void solution() {
+	for (int i = 0; i < n; i++) {
+		cin >> c;
+
+		if (c == 'A')
+			a++;
+		else
+			b++;
+	}
+
+	if (a > b)
+		cout << "A";
+	else if (a == b)
+		cout << "Tie";
+	else
+		cout << "B";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/733_Flood_Fill.cpp
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+	vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+		int dx[] = { 0, 0, 1, -1 };
+		int dy[] = { 1, -1, 0, 0 };
+
+		queue<pair<int, int>> q;
+
+		int original = image[sr][sc];
+
+		if (original == color)
+			return image;
+
+		q.push({ sr, sc });
+		image[sr][sc] = color;
+
+		while (!q.empty()) {
+			int x = q.front().first;
+			int y = q.front().second;
+			q.pop();
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (nx < 0 || ny < 0 || nx >= image.size() || ny >= image[0].size()) continue;
+
+				if (image[nx][ny] == original) {
+					image[nx][ny] = color;
+					q.push({ nx, ny });
+				}
+			}
+		}
+
+		return image;
+	}
+};
