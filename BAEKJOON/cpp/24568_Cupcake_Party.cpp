f98453fd0f0c0560d9ce1c9b9210fadc855a960c
hyee0715 <hyee0715@naver.com>
2022-07-15 18:02:55 +0900

    220715 upload

---
+++ BAEKJOON/cpp/24568_Cupcake_Party.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int r, s;
+
+void input() {
+	cin >> r >> s;
+}
+
+void solution() {
+	int sum = r * 8 + s * 3;
+
+	cout << sum - 28;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
