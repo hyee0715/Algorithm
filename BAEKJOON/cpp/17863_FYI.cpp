e050d3e863623b9b74d64f65f22ee2dfba5cce5b
hyee0715 <hyee0715@naver.com>
2022-07-16 12:34:45 +0900

    220716 upload

---
+++ BAEKJOON/cpp/17863_FYI.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	for (int i = 0; i < 3; i++) {
+		if (s[i] != '5') {
+			cout << "NO";
+			return;
+		}
+	}
+
+	cout << "YES";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
