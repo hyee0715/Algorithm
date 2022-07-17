5ffdcbe87e9f955538661747eee9274d9a649a1c
hyee0715 <hyee0715@naver.com>
2022-07-17 11:29:53 +0900

    220717 upload

---
+++ BAEKJOON/cpp/17009_Winning_Score.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int s1 = 0, s2 = 0;
+
+void input() {
+	int a, b, c;
+	
+	cin >> a >> b >> c;
+	s1 += a * 3;
+	s1 += b * 2;
+	s1 += c * 1;
+
+	cin >> a >> b >> c;
+	s2 += a * 3;
+	s2 += b * 2;
+	s2 += c * 1;
+}
+
+void solution() {
+	if (s1 > s2)
+		cout << "A";
+	else if (s1 < s2)
+		cout << "B";
+	else
+		cout << "T";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
