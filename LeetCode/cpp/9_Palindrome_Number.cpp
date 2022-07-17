496c5342c6f7310283ecc6322dd2ae52e5da5f76
hyee0715 <hyee0715@naver.com>
2022-07-17 22:08:40 +0900

    220717 upload

---
+++ LeetCode/cpp/9_Palindrome_Number.cpp
@@ -0,0 +1,13 @@
+﻿class Solution {
+public:
+	bool isPalindrome(int x) {
+		string s = to_string(x);
+		string rev_s = s;
+
+		reverse(rev_s.begin(), rev_s.end());
+
+		if (s == rev_s)
+			return true;
+		return false;
+	}
+};
