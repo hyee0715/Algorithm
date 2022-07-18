ad6fc9a66d70b8f0d525d3680b477d79bbdd4365
hyee0715 <hyee0715@naver.com>
2022-07-18 15:09:15 +0900

    220718 upload

---
+++ BAEKJOON/cpp/11319_Count_Me_In.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+int c, v;
+
+void input() {
+	getline(cin, s);
+}
+
+void solution() {
+	c = 0, v = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != 'A' && s[i] != 'E' && s[i] != 'I' && s[i] != 'O' && s[i] != 'U' && s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && s[i] != ' ') {
+			c++;
+		}
+		else {
+			if (s[i] != ' ')
+				v++;
+		}
+	}
+
+	cout << c << " " << v << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int tc;
+	cin >> tc;
+	cin.ignore();
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/2085_Count_Common_Words_With_One_Occurrence.cpp
@@ -0,0 +1,24 @@
+﻿class Solution {
+public:
+	int countWords(vector<string>& words1, vector<string>& words2) {
+		unordered_map<string, int> um1, um2;
+		int answer = 0;
+
+		for (int i = 0; i < words1.size(); i++) {
+			um1[words1[i]]++;
+		}
+
+		for (int i = 0; i < words2.size(); i++) {
+			um2[words2[i]]++;
+		}
+
+		for (auto pair : um1) {
+			if (pair.second == 1) {
+				if (um2[pair.first] == 1)
+					answer++;
+			}
+		}
+
+		return answer;
+	}
+};
