7ea299f0515d022881529572d2f3b05442bfbc0a
hyee0715 <hyee0715@naver.com>
2022-07-19 16:12:15 +0900

    220719 upload

---
+++ BAEKJOON/cpp/11367_Report_Card_Time.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s, grade;
+int n;
+
+void input() {
+	cin >> s >> n;
+}
+
+void solution() {
+	if (n >= 97)
+		grade = "A+";
+	else if (n >= 90)
+		grade = "A";
+	else if (n >= 87)
+		grade = "B+";
+	else if (n >= 80)
+		grade = "B";
+	else if (n >= 77)
+		grade = "C+";
+	else if (n >= 70)
+		grade = "C";
+	else if (n >= 67)
+		grade = "D+";
+	else if (n >= 60)
+		grade = "D";
+	else
+		grade = "F";
+
+	cout << s << " " << grade << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
