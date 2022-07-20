e773396ad8cfcd8b26a1b8a606dcea3f30a4e489
hyee0715 <hyee0715@naver.com>
2022-07-20 15:31:59 +0900

    220720 upload

---
+++ BAEKJOON/cpp/1362_펫.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int o, w, n;
+char c;
+int senario = 0;
+bool die;
+
+void solution() {
+	while (1) {
+		cin >> c >> n;
+		
+		if (c == '#' && n == 0)
+			break;
+
+		if (!die) {
+			if (c == 'E') // 운동
+				w -= n;
+			else if (c == 'F') // 먹이
+				w += n;
+		}
+
+		if (w <= 0)
+			die = true;
+	}
+
+	senario++;
+	if (w <= 0)
+		cout << senario << " RIP\n";
+	else if (w > o / 2 && w < o * 2)
+		cout << senario << " :-)\n";
+	else
+		cout << senario << " :-(\n";
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> o >> w;
+
+		if (o == 0 && w == 0)
+			break;
+
+		die = false;
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/58_Length_of_Last_Word.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		vector<string> v;
+		string temp;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] != ' ')
+				temp += s[i];
+			else {
+				if (temp != "") {
+					v.push_back(temp);
+					temp.clear();
+				}
+			}
+		}
+
+		if (temp != "")
+			v.push_back(temp);
+
+		return v[v.size() - 1].size();
+	}
+};
