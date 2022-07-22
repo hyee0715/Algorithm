e8bb0188c75ef2989d30ea121db65473b9780cc5
hyee0715 <hyee0715@naver.com>
2022-07-22 15:15:52 +0900

    220722 upload

---
+++ BAEKJOON/cpp/18398_HOMWRK.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, a, b;
+
+void solution() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+
+		cout << a + b << " " << a * b << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/234_Palindrome_Linked_List.cpp
@@ -0,0 +1,42 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	void explore_linked_list(ListNode* head, string& s) {
+		if (head == nullptr)
+			return;
+
+		s += to_string(head->val);
+		explore_linked_list(head->next, s);
+	}
+
+	bool find_palindrome(string s) {
+		int left = 0;
+		int right = s.size() - 1;
+
+		while (left < right) {
+			if (s[left++] != s[right--])
+				return false;
+		}
+
+		return true;
+	}
+
+	bool isPalindrome(ListNode* head) {
+		string s;
+		explore_linked_list(head, s);
+
+		if (find_palindrome(s))
+			return true;
+
+		return false;
+	}
+};
