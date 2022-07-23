971f4ab7278fb1b456fb658ed155dcdbc0f329a3
hyee0715 <hyee0715@naver.com>
2022-07-23 17:47:41 +0900

    220723 upload

---
+++ BAEKJOON/cpp/10950_A+B_-_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int a, b;
+
+void solution() {
+	cin >> a >> b;
+	
+	cout << a + b << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/328_Odd_Even_Linked_List.cpp
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* count_node(ListNode* head) {
+		ListNode* odd = new ListNode();
+		ListNode* even = new ListNode();
+		ListNode* odd_head = odd;
+		ListNode* even_head = even;
+		int cnt = 0;
+
+		while (head != nullptr) {
+			cnt++;
+
+			if (cnt % 2 != 0) {
+				odd->next = new ListNode(head->val);
+				odd = odd->next;
+			}
+			else {
+				even->next = new ListNode(head->val);
+				even = even->next;
+			}
+
+			head = head->next;
+		}
+
+		odd->next = even_head->next;
+
+		return odd_head->next;
+	}
+
+	ListNode* oddEvenList(ListNode* head) {
+		return count_node(head);
+	}
+};
