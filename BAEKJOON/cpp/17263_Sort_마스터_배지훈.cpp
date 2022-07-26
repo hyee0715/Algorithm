716a5ae963cd433d97b3f241099372bc442a5f81
hyee0715 <hyee0715@naver.com>
2022-07-26 18:27:13 +0900

    220726 upload

---
+++ BAEKJOON/cpp/17263_Sort_마스터_배지훈.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+vector<int> nums;
+
+void input() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		nums.push_back(a);
+	}
+}
+
+void solution() {
+	sort(nums.begin(), nums.end());
+
+	cout << nums[nums.size() - 1];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1957_Delete_Characters_to_Make_Fancy_String.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+	string makeFancyString(string s) {
+		string ret;
+
+		int cnt = 1;
+		char pre = s[0];
+		ret += pre;
+
+		for (int i = 1; i < s.size(); i++) {
+			if (s[i] == pre) {
+				cnt++;
+
+				if (cnt >= 3) {
+					continue;
+				}
+
+			}
+			else {
+				pre = s[i];
+				cnt = 1;
+			}
+
+			ret += s[i];
+		}
+
+		return ret;
+	}
+};
