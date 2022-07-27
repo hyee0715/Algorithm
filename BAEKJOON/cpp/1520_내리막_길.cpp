5f11ee038cc16293b12ac276357b5c82c7c17f28
hyee0715 <hyee0715@naver.com>
2022-07-27 17:04:18 +0900

    220727 upload

---
+++ BAEKJOON/cpp/1520_내리막_길.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int m, n;
+vector<vector<int>> map;
+vector<vector<int>> dp(500, vector<int>(500, -1));
+
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+
+void input() {
+	cin >> m >> n;
+
+	for (int i = 0; i < m; i++) {
+		vector<int> v;
+
+		for (int j = 0; j < n; j++) {
+			int a;
+			cin >> a;
+
+			v.push_back(a);
+		}
+
+		map.push_back(v);
+	}
+}
+
+int dfs(int x, int y) {
+	if (x == m - 1 && y == n - 1) return 1;
+	if (dp[x][y] != -1) return dp[x][y];
+
+	dp[x][y] = 0;
+
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+
+		if (map[x][y] > map[nx][ny]) {
+			dp[x][y] = dp[x][y] + dfs(nx, ny);
+		}
+	}
+
+	return dp[x][y];
+}
+
+void solution() {
+	cout << dfs(0, 0);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/965_Univalued_Binary_Tree.cpp
@@ -0,0 +1,34 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	bool find_univalued_tree(TreeNode* root, int& n) {
+		if (root == nullptr)
+			return true;
+
+		if (root->val == n) {
+			if (!find_univalued_tree(root->left, n) || !find_univalued_tree(root->right, n))
+				return false;
+		}
+		else
+			return false;
+
+		return true;
+	}
+
+	bool isUnivalTree(TreeNode* root) {
+		if (find_univalued_tree(root, root->val))
+			return true;
+
+		return false;
+	}
+};
