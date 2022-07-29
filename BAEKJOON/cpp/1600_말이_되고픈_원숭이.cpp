8cadcd396aa58a0111f4b8362341c62e818e7af0
hyee0715 <hyee0715@naver.com>
2022-07-29 16:48:11 +0900

    220729 upload

---
+++ BAEKJOON/cpp/1600_말이_되고픈_원숭이.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+#define MAX 201
+using namespace std;
+
+int w, h, k, cnt = 0;
+vector<vector<int> > map;
+bool visit[MAX][MAX][31]; // visit[a][b][c]에서 c는 원숭이가 말 처럼 이동할 수 있는 횟수
+
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
+
+int h_dx[] = { -2, -1, 1, 2, 2, 1, -1, -2 };
+int h_dy[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+void input() {
+	cin >> k >> w >> h;
+
+	for (int i = 0; i < h; i++) {
+		vector<int> v;
+
+		for (int j = 0; j < w; j++) {
+			int a;
+			cin >> a;
+			v.push_back(a);
+		}
+
+		map.push_back(v);
+	}
+}
+
+void bfs(int a, int b, int c) {
+	queue<pair<pair<int, int>, pair<int, int> > > q; // x좌표, y좌표, 이동 동작 수, 말 처럼 이동한 횟수
+	visit[a][b][0] = true;
+	q.push({ {a, b}, {c, 0} });
+
+	while (!q.empty()) {
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		int count = q.front().second.first;
+		int horse = q.front().second.second;
+		q.pop();
+
+		if (x == h - 1 && y == w - 1) {
+			cout << count;
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) { // 인접한 네 방향으로 이동
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+
+			if (map[nx][ny] == 0 && visit[nx][ny][horse] == false) {
+				q.push({ {nx, ny}, {count + 1, horse} });
+				visit[nx][ny][horse] = true;
+			}
+		}
+
+		for (int i = 0; i < 8; i++) { // 말의 움직임으로 여덟 방향 이동
+			int nx = x + h_dx[i];
+			int ny = y + h_dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+
+			if (map[nx][ny] == 0 && visit[nx][ny][horse + 1] == false && horse < k) {
+				q.push({ {nx, ny}, {count + 1, horse + 1} });
+				visit[nx][ny][horse + 1] = true;
+			}
+		}
+	}
+
+	cout << -1;
+}
+
+void solution() {
+	memset(visit, false, sizeof(visit));
+	bfs(0, 0, cnt);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+	
+	return 0;
+}
---
+++ LeetCode/cpp/47_Permutations_II.cpp
@@ -0,0 +1,40 @@
+﻿class Solution {
+public:
+	void dfs(int cnt, vector<int>& nums, vector<vector<int>>& answer, vector<int>& v, vector<bool>& visit) {
+		if (cnt == nums.size()) {
+			answer.push_back(v);
+		}
+		else {
+			for (int i = 0; i < nums.size(); i++) {
+				if (visit[i] == true) continue;
+				visit[i] = true;
+				v.push_back(nums[i]);
+				dfs(cnt + 1, nums, answer, v, visit);
+				v.pop_back();
+				visit[i] = false;
+			}
+		}
+	}
+
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		vector<vector<int>> answer;
+		vector<int> v;
+		vector<bool> visit(9, false);
+		set<vector<int>> st;
+
+		dfs(0, nums, answer, v, visit);
+
+		for (int i = 0; i < answer.size(); i++) {
+			if (st.find(answer[i]) == st.end())
+				st.insert(answer[i]);
+		}
+
+		answer.clear();
+
+		for (auto s : st) {
+			answer.push_back(s);
+		}
+
+		return answer;
+	}
+};
