0dc4d2e8e0ac302abc41c8ec2c5b15b90b595088
hyee0715 <hyee0715@naver.com>
2022-07-30 19:40:35 +0900

    220730 upload

---
+++ BAEKJOON/cpp/9723_Right_Triangle.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int a, b, c;
+string answer;
+
+void input() {
+	cin >> a >> b >> c;
+}
+
+void solution() {
+	if (a > b && a > c) {
+		if (b * b + c * c == a * a)
+			answer = "YES";
+		else
+			answer = "NO";
+	}
+	else if (b > a && b > c) {
+		if (a * a + c * c == b * b)
+			answer = "YES";
+		else
+			answer = "NO";
+	}
+	else if (c > a && c > b) {
+		if (a * a + b * b == c * c)
+			answer = "YES";
+		else
+			answer = "NO";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+		cout << "Case #" << i + 1 << ": " << answer << "\n";
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/205_Isomorphic_Strings.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+	bool isIsomorphic(string s, string t) {
+		unordered_map<char, char> um1, um2;
+		string temp;
+
+		for (int i = 0; i < s.size(); i++) {
+			um1[s[i]] = t[i];
+			um2[t[i]] = s[i];
+		}
+
+		for (int i = 0; i < s.size(); i++) {
+			temp += um1[s[i]];
+		}
+
+		if (temp != t)
+			return false;
+
+		temp.clear();
+
+		for (int j = 0; j < t.size(); j++) {
+			temp += um2[t[j]];
+		}
+
+		if (temp != s)
+			return false;
+
+		return true;
+	}
+};
