9f3b2dedbd644e31026a406ccf5ec8496520002a
hyee0715 <hyee0715@naver.com>
2022-07-31 18:35:07 +0900

    220731 upload

---
+++ BAEKJOON/cpp/3047_ABC.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+string s;
+int a, b, c; // a < b < c;
+
+void input() {
+	int n;
+
+	for (int i = 0; i < 3; i++) {
+		cin >> n;
+		v.push_back(n);
+	}
+
+	cin >> s;
+}
+
+void solution() {
+	sort(v.begin(), v.end());
+
+	a = v[0];
+	b = v[1];
+	c = v[2];
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'A')
+			cout << a;
+		else if (s[i] == 'B')
+			cout << b;
+		else
+			cout << c;
+
+		cout << " ";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2248_Intersection_of_Multiple_Arrays.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	vector<int> intersection(vector<vector<int>>& nums) {
+		unordered_map<int, int> um;
+		vector<int> answer;
+
+		for (int i = 0; i < nums.size(); i++) {
+			for (int j = 0; j < nums[i].size(); j++) {
+				um[nums[i][j]]++;
+			}
+		}
+
+		for (auto m : um) {
+			if (m.second == nums.size()) {
+				answer.push_back(m.first);
+			}
+		}
+
+		sort(answer.begin(), answer.end());
+		return answer;
+	}
+};
