14637fcb9ef93e79682765ec8b2e320adeda53a5
hyee0715 <hyee0715@naver.com>
2022-08-01 18:25:50 +0900

    220801 upload

---
+++ BAEKJOON/cpp/18883_N_M_찍기.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m, cnt = 1;
+
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (j == m - 1)
+				cout << cnt;
+			else
+				cout << cnt << " ";
+
+			cnt++;
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/74_Search_a_2D_Matrix.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		for (int i = matrix.size() - 1; i >= 0; i--) {
+			if (matrix[i][0] > target)
+				continue;
+
+			for (int j = 0; j < matrix[i].size(); j++) {
+				if (matrix[i][j] == target)
+					return true;
+			}
+		}
+
+		return false;
+	}
+};
