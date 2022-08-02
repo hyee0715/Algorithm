4e17d510eeede106736d47110adf646f78cca1db
hyee0715 <hyee0715@naver.com>
2022-08-02 09:40:27 +0900

    220802 upload

---
+++ BAEKJOON/cpp/15000_CAPS.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+//#include <algorithm>
+using namespace std;
+
+string s;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	for (int i = 0; i < s.size(); i++) {
+		s[i] = toupper(s[i]);
+	}
+
+	cout << s;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
