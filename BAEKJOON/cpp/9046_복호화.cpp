848fc88bce86d538bc62e8211d952b3e82f07192
hyee0715 <hyee0715@naver.com>
2022-08-04 12:37:44 +0900

    220804 upload

---
+++ BAEKJOON/cpp/9046_복호화.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string s;
+
+void input() {
+	getline(cin, s);
+}
+
+void solution() {
+	vector<int> alphabet(26, 0);
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			alphabet[s[i] - 'a']++;
+		}
+	}
+
+	vector<pair<int, char>> v;
+
+	for (int i = 0; i < alphabet.size(); i++) {
+		v.push_back({ alphabet[i], i + 'a' });
+	}
+
+	sort(v.begin(), v.end(), greater<>());
+	
+	if (v[0].first == v[1].first) {
+		cout << "?\n";
+	}
+	else {
+		cout << v[0].second << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	cin.ignore();
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
