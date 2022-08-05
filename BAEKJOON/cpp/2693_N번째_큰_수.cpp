4b5fbac434d3fa4c5f20b0c660091854c24bd1fe
hyee0715 <hyee0715@naver.com>
2022-08-05 16:08:24 +0900

    220805 upload

---
+++ BAEKJOON/cpp/2693_N번째_큰_수.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+
+void input() {
+	v.clear();
+
+	int n;
+	for (int i = 0; i < 10; i++) {
+		cin >> n;
+		v.push_back(n);
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end(), greater<>());
+
+	cout << v[2] << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/409_Longest_Palindrome.cpp
@@ -0,0 +1,33 @@
+﻿class Solution {
+public:
+	int longestPalindrome(string s) {
+		unordered_map<char, int> um;
+		int answer = 0;
+		int odd_cnt = 0;
+
+		for (int i = 0; i < s.size(); i++)
+			um[s[i]]++;
+
+		for (auto m : um) {
+			if (m.second % 2 == 0) {
+				answer += m.second;
+			}
+			else { //m.second % 2 != 0
+				if (m.second > 1) {
+					answer += (m.second - 1);
+					um[m.first] = 1;
+				}
+			}
+		}
+
+		for (auto m : um) {
+			if (m.second == 1)
+				odd_cnt++;
+		}
+
+		if (odd_cnt >= 1)
+			answer++;
+
+		return answer;
+	}
+};
