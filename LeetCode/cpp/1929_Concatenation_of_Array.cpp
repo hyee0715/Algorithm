1a0c9f2ef0259378d3916af6422e413c19b3ddf8
hyee0715 <hyee0715@naver.com>
2022-08-06 21:27:32 +0900

    220806 upload

---
+++ LeetCode/cpp/1929_Concatenation_of_Array.cpp
@@ -0,0 +1,14 @@
+﻿class Solution {
+public:
+	vector<int> getConcatenation(vector<int>& nums) {
+		vector<int> answer;
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < nums.size(); j++) {
+				answer.push_back(nums[j]);
+			}
+		}
+
+		return answer;
+	}
+};
---
+++ LeetCode/cpp/2351_First_Letter_to_Appear_Twice.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+	char repeatedCharacter(string s) {
+		unordered_map<char, int> um;
+		char answer;
+
+		for (int i = 0; i < s.size(); i++) {
+			um[s[i]]++;
+
+			if (um[s[i]] == 2) {
+				answer = s[i];
+				break;
+			}
+		}
+
+		return answer;
+	}
+};
