27d4f4c68eabaa496d3b7b6071dba718eb1694e6
hyee0715 <hyee0715@naver.com>
2022-08-07 16:47:24 +0900

    220807 upload

---
+++ BAEKJOON/cpp/2470_두_용액.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int n;
+long long ans1, ans2;
+vector<long long> nums;
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+
+		nums.push_back(a);
+	}
+}
+
+void solution() {
+	sort(nums.begin(), nums.end());
+
+	int left = 0;
+	int right = nums.size() - 1;
+	int result = 0x7fffffff;
+
+	while (left < right) {
+		int sum = nums[left] + nums[right];
+
+		if (abs(sum) < result) {
+			result = abs(sum);
+			ans1 = nums[left];
+			ans2 = nums[right];
+		
+			if (sum == 0)
+				break;
+		}
+
+		if (sum < 0) {
+			left++;
+		}
+		else {
+			right--;
+		}
+	}
+
+	cout << ans1 << " " << ans2;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1556_Thousand_Separator.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+	string thousandSeparator(int n) {
+		string s = to_string(n);
+		string answer = "";
+
+		int cnt = 0;
+
+		for (int i = s.size() - 1; i >= 0; i--) {
+			if (cnt == 3) {
+				cnt = 0;
+				answer = '.' + answer;
+
+			}
+
+			cnt++;
+			answer = s[i] + answer;
+
+		}
+
+		return answer;
+	}
+};
