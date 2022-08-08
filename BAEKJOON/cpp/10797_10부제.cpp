2a5e91d395e868b31d520116618598940c020abb
hyee0715 <hyee0715@naver.com>
2022-08-08 11:04:23 +0900

    220808 upload

---
+++ BAEKJOON/cpp/10797_10부제.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, answer = 0;
+
+void solution() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < 5; i++) {
+		cin >> a;
+
+		if (a == n)
+			answer++;
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/217_Contains_Duplicate.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+		unordered_map<int, int> um;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]]++;
+		}
+
+		for (auto m : um) {
+			if (m.second > 1)
+				return true;
+		}
+
+		return false;
+	}
+};
