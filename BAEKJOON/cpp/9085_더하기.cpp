7d8adbf9bebe3b012e2b6bb61922263916c42262
hyee0715 <hyee0715@naver.com>
2022-08-17 23:26:02 +0900

    220817 upload

---
+++ BAEKJOON/cpp/9085_더하기.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void solution() {
+	int n;
+	int sum = 0;
+
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		sum += a;
+	}
+
+	cout << sum << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/1582_Special_Positions_in_a_Binary_Matrix.cpp
@@ -0,0 +1,35 @@
+﻿class Solution {
+public:
+	bool check(vector<vector<int>>& mat, int a, int b) {
+		for (int i = 0; i < mat.size(); i++) {
+			if (i == a) continue;
+
+			if (mat[i][b] != 0)
+				return false;
+		}
+
+		for (int j = 0; j < mat[a].size(); j++) {
+			if (j == b) continue;
+
+			if (mat[a][j] != 0)
+				return false;
+		}
+
+		return true;
+	}
+
+	int numSpecial(vector<vector<int>>& mat) {
+		int answer = 0;
+
+		for (int i = 0; i < mat.size(); i++) {
+			for (int j = 0; j < mat[i].size(); j++) {
+				if (mat[i][j] == 1) {
+					if (check(mat, i, j))
+						answer++;
+				}
+			}
+		}
+
+		return answer;
+	}
+};
