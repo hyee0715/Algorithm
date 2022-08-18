8fe4f2500f3ac0663ea3257b1af20315860c5de7
hyee0715 <hyee0715@naver.com>
2022-08-18 20:26:34 +0900

    220818 upload

---
+++ BAEKJOON/cpp/5704_팬그램.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<int> alphabet;
+string s;
+
+bool check() {
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] == 0)
+			return false;
+	}
+
+	return true;
+}
+
+void solution() {
+	alphabet.assign(26, 0);
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ') continue;
+
+		alphabet[s[i] - 'a']++;
+	}
+
+	if (check())
+		cout << "Y\n";
+	else
+		cout << "N\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		getline(cin, s);
+
+		if (s == "*")
+			break;
+
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/540_Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+	int singleNonDuplicate(vector<int>& nums) {
+		unordered_map<int, int> um;
+		int answer = 0;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]]++;
+		}
+
+		for (auto m : um) {
+			if (m.second != 2) {
+				answer = m.first;
+			}
+		}
+
+		return answer;
+	}
+};
