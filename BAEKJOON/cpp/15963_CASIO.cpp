afac71b06d76459a1c67fccc781ae0e86bce659a
hyee0715 <hyee0715@naver.com>
2022-08-20 16:13:16 +0900

    220820 upload

---
+++ BAEKJOON/cpp/15963_CASIO.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+long long n, m;
+	
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	if (n == m)
+		cout << 1;
+	else
+		cout << 0;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+	
+	return 0;
+}
---
+++ LeetCode/cpp/1481_Least_Number_of_Unique_Integers_after_K_Removals.cpp
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+	int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+		vector<pair<int, int>> v;
+		unordered_map<int, int> um;
+		int answer = 0;
+
+		for (int i = 0; i < arr.size(); i++) {
+			um[arr[i]]++;
+		}
+
+		for (auto m : um) {
+			v.push_back({ m.second, m.first });
+		}
+
+		sort(v.begin(), v.end());
+
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i].first > k) {
+				v[i].first -= k;
+				k = 0;
+			}
+			else {
+				k -= v[i].first;
+				v[i].first = 0;
+			}
+		}
+
+		for (int i = 0; i < v.size(); i++) {
+			if (v[i].first != 0) {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+};
