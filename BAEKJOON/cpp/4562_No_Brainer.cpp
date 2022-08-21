28e4114e89acea5340c14e4cec0d56cf28bca889
hyee0715 <hyee0715@naver.com>
2022-08-21 17:27:50 +0900

    220821 upload

---
+++ BAEKJOON/cpp/4562_No_Brainer.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int x, y;
+
+void input() {
+	cin >> x >> y;
+}
+
+void solution() {
+	if (x >= y) {
+		cout << "MMM BRAINS\n";
+	}
+	else {
+		cout << "NO BRAINS\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/1859_Sorting_the_Sentence.cpp
@@ -0,0 +1,36 @@
+﻿class Solution {
+public:
+	string sortSentence(string s) {
+		vector<pair<int, string>> v;
+		string answer, temp;
+		int num;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == ' ') {
+				pair<int, string> p = { num, temp };
+				v.push_back(p);
+				temp.clear();
+			}
+			else if (s[i] >= '0' && s[i] <= '9') {
+				num = s[i] - '0';
+			}
+			else {
+				temp += s[i];
+			}
+		}
+
+		pair<int, string> p = { num, temp };
+		v.push_back(p);
+
+		sort(v.begin(), v.end());
+
+		for (int i = 0; i < v.size(); i++) {
+			answer += v[i].second;
+
+			if (i != v.size() - 1)
+				answer += ' ';
+		}
+
+		return answer;
+	}
+};
