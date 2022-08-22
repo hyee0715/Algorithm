fb9873a67d8c292d19e4091752b551165eaac000
hyee0715 <hyee0715@naver.com>
2022-08-22 22:36:46 +0900

    220822 upload

---
+++ BAEKJOON/cpp/10974_모든_순열.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int n;
+vector<int> v;
+bool visit[9];
+
+void input() {
+	cin >> n;
+}
+
+void dfs(int cnt) {
+	if (cnt == n) {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] == true) continue;
+			visit[i] = true;
+			v.push_back(i);
+			dfs(cnt + 1);
+			visit[i] = false;
+			v.pop_back();
+		}
+	}
+}
+
+void solution() {
+	memset(visit, false, sizeof(visit));
+	dfs(0);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2124_Check_if_All_A's_Appears_Before_All_B's.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	bool checkString(string s) {
+		bool flag = false;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'b')
+				flag = true;
+			else {
+				if (flag == true)
+					return false;
+			}
+		}
+
+		return true;
+	}
+};
