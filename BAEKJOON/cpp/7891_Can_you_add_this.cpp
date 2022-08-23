6ae87e85db92b796ae34b89bd89379d6c0107e31
hyee0715 <hyee0715@naver.com>
2022-08-23 23:37:00 +0900

    220823 upload

---
+++ BAEKJOON/cpp/7891_Can_you_add_this.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+long long x, y;
+
+void input() {
+	cin >> x >> y;
+}
+
+void solution() {
+	cout << x + y << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/345_Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,24 @@
+﻿class Solution {
+public:
+	string reverseVowels(string s) {
+		string t;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'a' || s[i] == 'i' || s[i] == 'u' || s[i] == 'e' || s[i] == 'o' || s[i] == 'A' || s[i] == 'I' || s[i] == 'U' || s[i] == 'E' || s[i] == 'O') {
+				t += s[i];
+			}
+		}
+
+		reverse(t.begin(), t.end());
+
+		int idx = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'a' || s[i] == 'i' || s[i] == 'u' || s[i] == 'e' || s[i] == 'o' || s[i] == 'A' || s[i] == 'I' || s[i] == 'U' || s[i] == 'E' || s[i] == 'O') {
+				s[i] = t[idx++];
+			}
+		}
+
+		return s;
+	}
+};
