abbf65ef0b411f09455bfa57e0da8d2e58594dbc
hyee0715 <hyee0715@naver.com>
2022-08-25 21:07:44 +0900

    220825 upload

---
+++ BAEKJOON/cpp/3046_R2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int r1, r2, s;
+
+void input() {
+	cin >> r1 >> s;
+}
+
+void solution() {
+	r2 = 2 * s - r1;
+
+	cout << r2;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1213_Intersection_of_Three_Sorted_Arrays.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+	vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
+		vector<int> answer;
+
+		for (int i = 0; i < arr1.size(); i++) {
+			if (find(arr2.begin(), arr2.end(), arr1[i]) != arr2.end()) {
+				if (find(arr3.begin(), arr3.end(), arr1[i]) != arr3.end()) {
+					answer.push_back(arr1[i]);
+				}
+			}
+		}
+
+		return answer;
+	}
+};
