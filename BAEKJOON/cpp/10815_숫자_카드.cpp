29c93ff8009190b2922a75f92c0e380ed149c2e3
hyee0715 <hyee0715@naver.com>
2022-08-27 09:29:40 +0900

    220827 upload

---
+++ BAEKJOON/cpp/10815_숫자_카드.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int n, m;
+set<int> st;
+vector<int> v1, v2;
+
+void input() {
+	cin >> n;
+	
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v1.push_back(a);
+	}
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> a;
+		v2.push_back(a);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < v1.size(); i++) {
+		st.insert(v1[i]);
+	}
+
+	for (int i = 0; i < v2.size(); i++) {
+		if (st.find(v2[i]) != st.end()) {
+			cout << "1 ";
+		}
+		else {
+			cout << "0 ";
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/360_Sort_Transformed_Array.cpp
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+	vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
+		vector<int> answer;
+
+		for (int i = 0; i < nums.size(); i++) {
+			int result = a * nums[i] * nums[i] + b * nums[i] + c;
+			answer.push_back(result);
+		}
+
+		sort(answer.begin(), answer.end());
+
+		return answer;
+	}
+};
