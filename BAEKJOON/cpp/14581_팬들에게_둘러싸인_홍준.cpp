1903371a79502faaa8def2ee9e57cd2d87424c94
hyee0715 <hyee0715@naver.com>
2022-08-28 16:21:57 +0900

    220828 upload

---
+++ BAEKJOON/cpp/14581_팬들에게_둘러싸인_홍준.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (i == 1 && j == 1) {
+				cout << ":" << s << ":";
+			}
+			else {
+				cout << ":fan:";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/404_Sum_of_Left_Leaves.cpp
@@ -0,0 +1,35 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	void find_sum(TreeNode* root, int& sum, bool is_left) {
+		if (root == nullptr) {
+			return;
+		}
+
+		if (root->left == nullptr && root->right == nullptr && is_left) {
+			sum += root->val;
+			return;
+		}
+
+		find_sum(root->left, sum, true);
+		find_sum(root->right, sum, false);
+	}
+
+	int sumOfLeftLeaves(TreeNode* root) {
+		int sum = 0;
+		bool is_left = false;
+		find_sum(root, sum, is_left);
+
+		return sum;
+	}
+};
