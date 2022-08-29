9d7a5f471d57fbc3da6d8cc9ba243bbb7433f053
hyee0715 <hyee0715@naver.com>
2022-08-29 12:10:35 +0900

    220829 upload

---
+++ BAEKJOON/cpp/2738_행렬_덧셈.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> v1, v2;
+int n, m;
+
+void input() {
+	cin >> n >> m;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		vector<int> temp;
+
+		for (int j = 0; j < m; j++) {
+			cin >> a;
+			temp.push_back(a);
+		}
+
+		v1.push_back(temp);
+	}
+
+	for (int i = 0; i < n; i++) {
+		vector<int> temp;
+
+		for (int j = 0; j < m; j++) {
+			cin >> a;
+			temp.push_back(a);
+		}
+
+		v2.push_back(temp);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << v1[i][j] + v2[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2108_Find_First_Palindromic_String_in_the_Array.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+	string firstPalindrome(vector<string>& words) {
+		string answer;
+		bool found;
+
+		for (int i = 0; i < words.size(); i++) {
+			int left = 0;
+			int right = words[i].size() - 1;
+			found = true;
+
+			while (left <= right) {
+				if (words[i][left] != words[i][right]) {
+					found = false;
+					break;
+				}
+
+				left++;
+				right--;
+			}
+
+			if (found) {
+				answer = words[i];
+				break;
+			}
+		}
+
+		return answer;
+	}
+};
