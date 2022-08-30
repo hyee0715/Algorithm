ed0673e352a7c724e314a02a57c53bb8ab0744fe
hyee0715 <hyee0715@naver.com>
2022-08-30 11:32:42 +0900

    220830 upload

---
+++ BAEKJOON/cpp/5554_심부름_가는_길.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int time = 0;
+
+void input() {
+	int n;
+
+	for (int i = 0; i < 4; i++) {
+		cin >> n;
+
+		time += n;
+	}
+}
+
+void solution() {
+	cout << time / 60 << "\n" << time % 60;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2239_Find_Closest_Number_to_Zero.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	int findClosestNumber(vector<int>& nums) {
+		int answer = -100001;
+		int min_num = 100001;
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (abs(nums[i]) == min_num) {
+				min_num = abs(nums[i]);
+				answer = max(answer, nums[i]);
+			}
+			else if (abs(nums[i]) < min_num) {
+				min_num = abs(nums[i]);
+				answer = nums[i];
+			}
+		}
+
+		return answer;
+	}
+};
