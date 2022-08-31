db3ef3fce433b0883f9079b4a6d6eb9a774b3888
hyee0715 <hyee0715@naver.com>
2022-08-31 18:51:19 +0900

    220831 upload

---
+++ BAEKJOON/cpp/5596_시험_점수.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int minkook = 0, manse = 0;
+int a, b, c, d;
+
+void solution() {
+	for (int i = 0; i < 2; i++) {
+		cin >> a >> b >> c >> d;
+	
+		if (i == 0) {
+			minkook = a + b + c + d;
+		}
+		else {
+			manse = a + b + c + d;
+		}
+	}
+
+	if (minkook >= manse) {
+		cout << minkook;
+	}
+	else {
+		cout << manse;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+	vector<long long> sumOfThree(long long num) {
+		vector<long long> answer;
+
+		if ((num - 3) % 3 == 0) {
+			long long n = (num - 3) / 3;
+
+			answer.push_back(n);
+			answer.push_back(n + 1);
+			answer.push_back(n + 2);
+		}
+
+		return answer;
+	}
+};
