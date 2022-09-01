dfc5ed79f87b1fcc5bb9f32f08449a3623f3ff4c
hyee0715 <hyee0715@naver.com>
2022-09-01 18:24:15 +0900

    220901 upload

---
+++ BAEKJOON/cpp/5532_방학_숙제.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int l, a, b, c, d;
+
+void input() {
+	cin >> l >> a >> b >> c >> d;
+}
+
+void solution() {
+	int lang = a / c;
+	int math = b / d;
+
+	if (a % c != 0)
+		lang++;
+
+	if (b % d != 0)
+		math++;
+
+	if (lang > math) 
+		cout << l - lang;
+	else 
+		cout << l - math;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1287_Element_Appearing_More_Than_25_In_Sorted_Array.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+	int findSpecialInteger(vector<int>& arr) {
+		map<int, int> um;
+		int answer;
+		int max_num = -1;
+
+		for (int i = 0; i < arr.size(); i++) {
+			um[arr[i]]++;
+		}
+
+		for (auto m : um) {
+			if (m.second > max_num) {
+				max_num = m.second;
+				answer = m.first;
+			}
+		}
+
+		return answer;
+	}
+};
