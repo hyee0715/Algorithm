dc5c22a2d65edaef211bd524c9ed247fdd50d82e
hyee0715 <hyee0715@naver.com>
2022-09-02 16:12:52 +0900

    220902 upload

---
+++ BAEKJOON/cpp/11022_A+B_-_8.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int a, b, sum;
+
+void input() {
+	cin >> a >> b;
+}
+
+void solution() {
+	sum = a + b;
+	
+	cout << a << " + " << b << " = " << sum << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int  tc;
+	cin >> tc;
+
+	for (int i = 1; i <= tc; i++) {
+		input();
+
+		cout << "Case #" << i << ": ";
+		solution();
+	}
+	
+	return 0;
+}
