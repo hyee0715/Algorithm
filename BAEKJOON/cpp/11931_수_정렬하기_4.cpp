f94ed39ada37da653705e31247b561a5fec6e3a5
hyee0715 <hyee0715@naver.com>
2022-09-04 14:57:47 +0900

    220904 upload

---
+++ BAEKJOON/cpp/11931_수_정렬하기_4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<int> v;
+
+void input() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+
+		v.push_back(a);
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end(), greater<>());
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+	
+	return 0;
+}
---
+++ LeetCode/cpp/2341_Maximum_Number_of_Pairs_in_Array.cpp
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+	vector<int> numberOfPairs(vector<int>& nums) {
+		unordered_map<int, int> um;
+		int even_cnt = 0;
+		int leftover = 0;
+		vector<int> answer;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]]++;
+		}
+
+		for (auto m : um) {
+			while (m.second > 1) {
+				um[m.first] = m.second - 2;
+				m.second = m.second - 2;
+
+				even_cnt++;
+			}
+
+			if (m.second % 2 != 0) {
+				leftover++;
+			}
+		}
+
+		answer.push_back(even_cnt);
+		answer.push_back(leftover);
+
+		return answer;
+	}
+};
