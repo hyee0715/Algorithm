8f7d25283965bb38ed43bcab4e4860a9da101686
hyee0715 <hyee0715@naver.com>
2022-09-05 10:32:00 +0900

    220905 upload

---
+++ BAEKJOON/cpp/3273_두_수의_합.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int n, x, cnt = 0;
+vector<int> v;
+set<int> s;
+
+void input() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+
+	cin >> x;
+}
+
+void solution() {
+	for (int i = 0; i < v.size(); i++) {
+		if (s.find(v[i]) != s.end()) {
+			cnt++;
+		}
+		else {
+			s.insert(x - v[i]);
+		}
+	}
+
+	cout << cnt;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+	
+	return 0;
+}
---
+++ LeetCode/cpp/896_Monotonic_Array.cpp
@@ -0,0 +1,34 @@
+﻿class Solution {
+public:
+	bool isMonotonic(vector<int>& nums) {
+		bool isIncreasing;
+
+		if (nums.size() < 2)
+			return true;
+
+		for (int i = 1; i < nums.size(); i++) {
+			if (nums[i - 1] == nums[i]) continue;
+
+			if (nums[i - 1] < nums[i]) {
+				isIncreasing = true;
+				break;
+			}
+			else {
+				isIncreasing = false;
+				break;
+			}
+		}
+
+		for (int i = 1; i < nums.size(); i++) {
+			if (isIncreasing && nums[i - 1] > nums[i]) {
+				return false;
+			}
+
+			if (!isIncreasing && nums[i - 1] < nums[i]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
