ebf84ef42d0e012d43063125b2e272478c765058
hyee0715 <hyee0715@naver.com>
2022-09-07 19:50:09 +0900

    220907 upload

---
+++ LeetCode/cpp/268_Missing_Number.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	int missingNumber(vector<int>& nums) {
+		int n = 0, answer;
+
+		sort(nums.begin(), nums.end());
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (n != nums[i]) {
+				answer = n;
+				break;
+			}
+			else {
+				n++;
+			}
+		}
+
+		return answer;
+	}
+};
