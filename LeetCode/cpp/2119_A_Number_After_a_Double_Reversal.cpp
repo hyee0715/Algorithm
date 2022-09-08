7ec679d0a3a48d9cfa42bc53972ec67ff2c8d5d9
hyee0715 <hyee0715@naver.com>
2022-09-08 23:11:50 +0900

    220908 upload

---
+++ LeetCode/cpp/2119_A_Number_After_a_Double_Reversal.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+	bool isSameAfterReversals(int num) {
+		string s = to_string(num);
+
+		if (s.size() == 1)
+			return true;
+
+		string r = s;
+		reverse(r.begin(), r.end());
+
+		while (1) {
+			if (r[0] == '0') {
+				r.erase(0, 1);
+			}
+			else {
+				break;
+			}
+		}
+
+		reverse(r.begin(), r.end());
+
+		if (r == s)
+			return true;
+		return false;
+	}
+};
