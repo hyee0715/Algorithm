7b0823c1af09a6908096f85456b41760bb26c1e5
hyee0715 <hyee0715@naver.com>
2022-09-08 20:40:10 +0900

    220908 upload

---
+++ BAEKJOON/cpp/1000_A+B.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int s, m, l;
+
+void input() {
+	cin >> s >> m >> l;
+}
+
+void solution() {
+	int sum = 1 * s + 2 * m + 3 * l;
+
+	if (sum >= 10)
+		cout << "happy";
+	else
+		cout << "sad";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
