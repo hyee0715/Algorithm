bf523f771889b3bd3e8e440d2718d4c8824ec179
hyee0715 <hyee0715@naver.com>
2022-09-09 23:38:45 +0900

    220909 upload

---
+++ BAEKJOON/cpp/14038_Tournament_Selection.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<char> v;
+
+void input() {
+	char c;
+
+	for (int i = 0; i < 6; i++) {
+		cin >> c;
+		v.push_back(c);
+	}
+}
+
+void solution() {
+	int sum = 0;
+
+	for (int i = 0; i < 6; i++) {
+		if (v[i] == 'W') {
+			sum++;
+		}
+	}
+
+	if (sum >= 5) {
+		cout << "1";
+	}
+	else if (sum >= 3) {
+		cout << "2";
+	}
+	else if (sum >= 1) {
+		cout << "3";
+	}
+	else {
+		cout << "-1";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
