c00120b98e07be277f726c6932bf031f7354a3ce
hyee0715 <hyee0715@naver.com>
2022-09-10 22:58:44 +0900

    220910 upload

---
+++ BAEKJOON/cpp/8370_Plane.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum = 0, a, b;
+
+void solution() {
+	for (int i = 0; i < 2; i++) {
+		cin >> a >> b;
+
+		sum += a * b;
+	}
+
+	cout << sum;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/961_N-Repeated_Element_in_Size_2N_Array.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	int repeatedNTimes(vector<int>& nums) {
+		unordered_map<int, int> um;
+		int answer = -1;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]]++;
+		}
+
+		for (auto m : um) {
+			if (m.second != 1) {
+				answer = m.first;
+				break;
+			}
+		}
+
+		return answer;
+	}
+};
