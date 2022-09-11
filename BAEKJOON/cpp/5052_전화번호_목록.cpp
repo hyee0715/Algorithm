4ddf5078c6543b3153072beffd5514e152099651
hyee0715 <hyee0715@naver.com>
2022-09-11 15:06:59 +0900

    220911 upload

---
+++ BAEKJOON/cpp/5052_전화번호_목록.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+using namespace std;
+
+int n;
+string a;
+vector<string> nums;
+set<string> st;
+
+bool compare(string a, string b) {
+	if (a.size() < b.size()) {
+		return true;
+	}
+	return false;
+}
+
+void initialize() {
+	nums.clear();
+	st.clear();
+}
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		nums.push_back(a);
+	}
+}
+
+void solution() {
+	sort(nums.begin(), nums.end(), compare);
+
+	for (int i = 0; i < nums.size(); i++) {
+		string s;
+
+		for (int j = 0; j < nums[i].size(); j++) {
+			s += nums[i][j];
+
+			if (st.find(s) != st.end()) {
+				cout << "NO\n";
+				return;
+			}
+		}
+
+		st.insert(s);
+	}
+
+	cout << "YES\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		initialize();
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/414_Third_Maximum_Number.cpp
@@ -0,0 +1,27 @@
+﻿class Solution {
+public:
+	int thirdMax(vector<int>& nums) {
+		sort(nums.begin(), nums.end(), greater<>());
+
+		int cnt = 1, answer;
+		int cur_max = nums[0];
+
+		for (int i = 1; i < nums.size(); i++) {
+			if (nums[i] < cur_max) {
+				cur_max = nums[i];
+				cnt++;
+			}
+
+			if (cnt == 3) {
+				answer = cur_max;
+				break;
+			}
+		}
+
+		if (cnt < 3) {
+			answer = nums[0];
+		}
+
+		return answer;
+	}
+};
