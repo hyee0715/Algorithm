6d7a10fba444b13ec96fc3dd040245994a242094
hyee0715 <hyee0715@naver.com>
2022-09-12 14:17:00 +0900

    220912 upload

---
+++ BAEKJOON/cpp/2083_럭비_클럽.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string name;
+int age, kg;
+
+void solution() {
+	if (age > 17 || kg >= 80) {
+		cout << name << " Senior\n";
+	}
+	else {
+		cout << name << " Junior\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> name >> age >> kg;
+
+		if (name == "#" && age == 0 && kg == 0) break;
+
+		solution();
+
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/2057_Smallest_Index_With_Equal_Value.cpp
@@ -0,0 +1,14 @@
+﻿class Solution {
+public:
+	int smallestEqual(vector<int>& nums) {
+		int answer = -1;
+		for (int i = 0; i < nums.size(); i++) {
+			if (i % 10 == nums[i] % 10) {
+				answer = i;
+				break;
+			}
+		}
+
+		return answer;
+	}
+};
