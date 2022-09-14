71da33dc248f0c9c29a4e42a71fe117f386b120a
hyee0715 <hyee0715@naver.com>
2022-09-14 11:51:48 +0900

    220914 upload

---
+++ BAEKJOON/cpp/2230_수_고르기.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n, m, min_num = INT_MAX;
+vector<int> v;
+
+void input() {
+	cin >> n >> m;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end());
+
+	int left = 0, right = 0;
+
+	while (left < n && right < n) {
+		if (v[right] - v[left] < m) {
+			right++;
+		}
+		else {
+			min_num = min(min_num, v[right] - v[left]);
+			left++;
+		}
+	}
+
+	cout << min_num;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/821_Shortest_Distance_to_a_Character.cpp
@@ -0,0 +1,39 @@
+﻿class Solution {
+public:
+	vector<int> shortestToChar(string s, char c) {
+		vector<int> answer(s.size(), 10001);
+		int cnt = 0;
+		bool flag = false;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == c) {
+				flag = true;
+				cnt = 0;
+				answer[i] = cnt;
+			}
+
+			if (s[i] != c && flag) {
+				cnt++;
+				answer[i] = cnt;
+			}
+		}
+
+		cnt = 0;
+		flag = false;
+
+		for (int i = s.size() - 1; i >= 0; i--) {
+			if (s[i] == c) {
+				flag = true;
+				cnt = 0;
+				answer[i] = cnt;
+			}
+
+			if (s[i] != c && flag) {
+				cnt++;
+				answer[i] = min(answer[i], cnt);
+			}
+		}
+
+		return answer;
+	}
+};
