a53c54a1ab8852b58dd1e363da93e1df503fa458
hyee0715 <hyee0715@naver.com>
2022-09-15 21:59:31 +0900

    220915 upload

---
+++ BAEKJOON/cpp/11382_꼬마_정민.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+long long a, b, c;
+
+void input() {
+	cin >> a >> b >> c;
+}
+
+void solution() {
+	cout << a + b + c;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1108_Defanging_an_IP_Address.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	string defangIPaddr(string address) {
+		string answer;
+
+		for (int i = 0; i < address.size(); i++) {
+			if (address[i] == '.') {
+				answer += "[.]";
+			}
+			else {
+				answer += address[i];
+			}
+		}
+
+		return answer;
+	}
+};
