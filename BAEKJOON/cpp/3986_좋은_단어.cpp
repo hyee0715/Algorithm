2cd85c4fed907944099e3d0dbe47635d5ef6a99b
hyee0715 <hyee0715@naver.com>
2022-09-16 19:02:09 +0900

    220916 upload

---
+++ BAEKJOON/cpp/3986_좋은_단어.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int n, cnt = 0;
+stack<char> st;
+vector<string> v;
+
+void input() {
+	cin >> n;
+	
+	string s;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		v.push_back(s);
+	}
+}
+
+void st_clear() {
+	while (!st.empty()) {
+		st.pop();
+	}
+}
+
+void solution() {
+	for (int i = 0; i < v.size(); i++) {
+		st_clear();
+
+		for (int j = 0; j < v[i].size(); j++) {
+			if (st.empty()) {
+				st.push(v[i][j]);
+			}
+			else if (st.top() != v[i][j]) {
+				st.push(v[i][j]);
+			}
+			else {
+				st.pop();
+			}
+		}
+
+		if (st.empty())
+			cnt++;
+	}
+
+	cout << cnt;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/219_Contains_Duplicate_II.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		unordered_map<int, vector<int>> um;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]].push_back(i);
+		}
+
+		for (auto m : um) {
+			if (m.second.size() >= 2) {
+				for (int i = 1; i < m.second.size(); i++) {
+					if (abs(m.second[i] - m.second[i - 1]) <= k) {
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+};
