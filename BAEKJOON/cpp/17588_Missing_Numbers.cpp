1138a8337a1d816d4fa0a9ebba29dec3d232ec26
hyee0715 <hyee0715@naver.com>
2022-09-17 18:10:33 +0900

    220917 upload

---
+++ BAEKJOON/cpp/17588_Missing_Numbers.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, max_num = -1;
+vector<bool> nums(201, false);
+bool flag = false;
+
+void solution() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+
+		nums[a] = true;
+
+		if (max_num <= a) {
+			max_num = a;
+		}
+	}
+
+	for (int i = 1; i <= max_num; i++) {
+		if (nums[i] == false) {
+			cout << i << "\n";
+			flag = true;
+		}
+	}
+
+	if (!flag)
+		cout << "good job";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1832_Check_if_the_Sentence_Is_Pangram.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	bool checkIfPangram(string sentence) {
+		vector<bool> alphabet(26, false);
+
+		for (int i = 0; i < sentence.size(); i++) {
+			alphabet[sentence[i] - 'a'] = true;
+		}
+
+		for (int i = 0; i < alphabet.size(); i++) {
+			if (alphabet[i] == false)
+				return false;
+		}
+
+		return true;
+	}
+};
