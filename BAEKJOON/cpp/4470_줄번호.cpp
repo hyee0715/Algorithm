51afab27c8c4501519134cc2e61dbade0346ea38
hyee0715 <hyee0715@naver.com>
2022-09-18 15:44:37 +0900

    220918 upload

---
+++ BAEKJOON/cpp/4470_줄번호.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+
+void input() {
+	cin >> n;
+}
+
+void solution() {
+	cin.ignore();
+
+	string s;
+
+	for (int i = 1; i <= n; i++) {
+		getline(cin, s);
+
+		cout << i << ". " << s << "\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/872_Leaf-Similar_Trees.cpp
@@ -0,0 +1,43 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	void check(TreeNode* root, vector<int>& v) {
+		if (root == nullptr)
+			return;
+
+		if (root->left == nullptr && root->right == nullptr)
+			v.push_back(root->val);
+
+		check(root->left, v);
+		check(root->right, v);
+	}
+
+	bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+		vector<int> v1, v2;
+
+		check(root1, v1);
+		check(root2, v2);
+
+		for (int i = 0; i < v1.size(); i++) {
+			if (v1[i] != v2[i])
+				return false;
+		}
+
+		for (int i = 0; i < v2.size(); i++) {
+			if (v1[i] != v2[i])
+				return false;
+		}
+
+		return true;
+	}
+};
