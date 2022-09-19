9421b0e7383880ded937d915ea64fa429486ab9e
hyee0715 <hyee0715@naver.com>
2022-09-19 21:25:55 +0900

    220919 upload

---
+++ BAEKJOON/cpp/10824_네_수.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string a, b, c, d, s1, s2;
+
+void input() {
+	cin >> a >> b >> c >> d;
+}
+
+void solution() {
+	s1 = a + b;
+	s2 = c + d;
+
+	long long n1 = stoll(s1);
+	long long n2 = stoll(s2);
+
+	cout << n1 + n2;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
