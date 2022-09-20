48e4c679a68a6a1834292ea1d236136564dba919
hyee0715 <hyee0715@naver.com>
2022-09-20 23:21:33 +0900

    220930 upload

---
+++ BAEKJOON/cpp/15726_이칙연산.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+long long a, b, c;
+
+void input() {
+	cin >> a >> b >> c;
+}
+
+void solution() {
+	long long result1 = (double)(a * b) / c;
+	long long result2 = ((double)a / b) * c;
+
+	if (result1 > result2) {
+		cout << result1;
+	} else {
+		cout << result2;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
