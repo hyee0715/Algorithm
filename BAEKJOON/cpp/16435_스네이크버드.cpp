4ccdab0edc879c6c5368b1383e12792ce1ba2a8e
hyee0715 <hyee0715@naver.com>
2022-09-21 10:24:17 +0900

    220921 upload

---
+++ BAEKJOON/cpp/16435_스네이크버드.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, l;
+vector<int> v;
+bool eat = false;
+
+void input() {
+	cin >> n >> l;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+}
+
+void solution() {
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i] <= l) {
+			l++;
+			eat = true;
+		}
+		else {
+			if (eat)
+				break;
+		}
+	}
+
+	cout << l;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1437_Check_If_All_1's_Are_at_Least_Length_K_Places_Away.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	bool kLengthApart(vector<int>& nums, int k) {
+		int cnt = 0;
+		bool flag = false;
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] == 1) {
+				if (cnt < k && flag)
+					return false;
+
+				flag = true;
+				cnt = 0;
+			}
+			else {
+				cnt++;
+			}
+		}
+
+		return true;
+	}
+};
