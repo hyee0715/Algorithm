30fd0602b460774be8ed0e592dae1bac347c7dbd
hyee0715 <hyee0715@naver.com>
2022-09-22 12:26:49 +0900

    220922 upload

---
+++ BAEKJOON/cpp/2506_점수계산.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, answer = 0;
+vector<int> v;
+
+void input() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+}
+
+void solution() {
+	bool prev = false;
+	int cnt = 1;
+
+	for (int i = 0; i < n; i++) {
+		if (v[i] == 1) {
+			if (prev) {
+				cnt++;
+			}
+
+			prev = true;
+			answer += cnt;
+
+		}
+		else {
+			cnt = 1;
+			prev = false;
+		}
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
