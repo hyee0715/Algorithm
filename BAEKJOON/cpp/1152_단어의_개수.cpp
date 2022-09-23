422ef4d729a4227ea0ad491332c2aa3925a73fa6
hyee0715 <hyee0715@naver.com>
2022-09-23 12:16:17 +0900

    220923 upload

---
+++ BAEKJOON/cpp/1152_단어의_개수.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string s;
+
+void input() {
+	getline(cin, s);
+}
+
+void solution() {
+	int count = 0;
+
+	if (s.empty()) {
+		cout << 0;
+		return;
+	}
+
+	count = 1;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ')
+			count++;
+	}
+
+	if (s[0] == ' ')
+		count--;
+
+	if (s[s.size() - 1] == ' ')
+		count--;
+
+	cout << count;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1394_Find_Lucky_Integer_in_an_Array.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	bool divideArray(vector<int>& nums) {
+		if (nums.size() % 2 != 0)
+			return false;
+
+		unordered_map<int, int> um;
+
+		for (int i = 0; i < nums.size(); i++) {
+			um[nums[i]]++;
+		}
+
+		for (auto m : um) {
+			if (m.second % 2 != 0)
+				return false;
+		}
+
+		return true;
+	}
+};
