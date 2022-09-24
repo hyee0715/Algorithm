08cd6efe73c158ea21b04220388a608bb3042ea6
hyee0715 <hyee0715@naver.com>
2022-09-24 14:34:23 +0900

    220924 upload

---
+++ BAEKJOON/cpp/17094_Serious_Problem.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string s;
+
+void input() {
+	cin >> n >> s;
+}
+
+void solution() {
+	int cnt_e = 0;
+	int cnt_2 = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (s[i] == '2')
+			cnt_2++;
+		else
+			cnt_e++;
+	}
+
+	if (cnt_2 > cnt_e)
+		cout << "2";
+	else if (cnt_2 < cnt_e)
+		cout << "e";
+	else
+		cout << "yee";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1196_How_Many_Apples_Can_You_Put_into_the_Basket.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+	int maxNumberOfApples(vector<int>& weight) {
+		int sum = 0;
+		int answer = 0;
+
+		sort(weight.begin(), weight.end());
+
+		for (int i = 0; i < weight.size(); i++) {
+			if ((sum + weight[i]) <= 5000) {
+				answer++;
+				sum += weight[i];
+			}
+		}
+
+		return answer;
+	}
+};
