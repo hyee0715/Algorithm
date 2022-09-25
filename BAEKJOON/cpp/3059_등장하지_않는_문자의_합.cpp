1baa777bc37f75a8793b2687038f5bf4446763f2
hyee0715 <hyee0715@naver.com>
2022-09-25 10:59:01 +0900

    220925 upload

---
+++ BAEKJOON/cpp/3059_등장하지_않는_문자의_합.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string s;
+vector<int> alphabet;
+int answer;
+
+void initialize() {
+	alphabet.assign(26, 0);
+	answer = 0;
+}
+
+void input() {
+	cin >> s;
+}
+
+void solution() {
+	for (int i = 0; i < s.size(); i++) {
+		alphabet[s[i] - 'A']++;
+	}
+
+	for (int i = 0; i < alphabet.size(); i++) {
+		if (alphabet[i] == 0) {
+			answer += (i + 65);
+		}
+	}
+
+	cout << answer << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		initialize();
+		input();
+		solution();
+	}
+
+	return 0;
+}
