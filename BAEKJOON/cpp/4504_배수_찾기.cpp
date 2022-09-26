deecf06a73b4beba048c5d6e57e9b134485c27c3
hyee0715 <hyee0715@naver.com>
2022-09-26 11:44:55 +0900

    220906 upload

---
+++ BAEKJOON/cpp/4504_배수_찾기.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+
+void solution() {
+	if (m % n != 0) {
+		cout << m << " is NOT a multiple of " << n << ".\n";
+	}
+	else {
+		cout << m << " is a multiple of " << n << ".\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> n;
+
+	while (1) {
+		cin >> m;
+
+		if (m == 0) break;
+
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/104_Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	void check(TreeNode* root, int depth, int& answer) {
+		if (root == nullptr)
+			return;
+
+		if (root->left == nullptr && root->right == nullptr) {
+			answer = max(answer, depth);
+		}
+
+		check(root->left, depth + 1, answer);
+		check(root->right, depth + 1, answer);
+	}
+
+	int maxDepth(TreeNode* root) {
+		int answer = 0;
+
+		check(root, 1, answer);
+
+		return answer;
+	}
+};
