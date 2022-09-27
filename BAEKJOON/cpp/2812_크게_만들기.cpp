56ec233861bc2645480e68404fdafbc16115ae1e
hyee0715 <hyee0715@naver.com>
2022-09-27 18:46:43 +0900

    220927 upload

---
+++ BAEKJOON/cpp/2812_크게_만들기.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int n, k;
+vector<int> v;
+string s;
+
+void input() {
+	cin >> n >> k >> s;
+}
+
+void solution() {
+	for (int i = 0; i < s.size(); i++) {
+		if (v.empty()) {
+			v.push_back(s[i] - '0');
+		}
+		else {
+			if (v.back() < (s[i] - '0') && k > 0) {
+				v.pop_back();
+				k--;
+			}
+
+			while (!v.empty()) {
+				if (v.back() < (s[i] - '0') && k > 0) {
+					v.pop_back();
+					k--;
+				}
+				else {
+					break;
+				}
+			}
+
+			v.push_back(s[i] - '0');
+		}
+	}
+
+	while (k > 0) {
+		v.pop_back();
+		k--;
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i];
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2418_Sort_the_People.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+	vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+		vector<pair<int, string>> v;
+		vector<string> answer;
+
+		for (int i = 0; i < names.size(); i++) {
+			v.push_back({ heights[i], names[i] });
+		}
+
+		sort(v.begin(), v.end(), greater<>());
+
+		for (int i = 0; i < v.size(); i++) {
+			answer.push_back(v[i].second);
+		}
+
+		return answer;
+	}
+};
