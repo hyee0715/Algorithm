6c1f022af3c9b48a21f56ddfab63cea725d1d56f
hyee0715 <hyee0715@naver.com>
2022-09-30 19:28:04 +0900

    220930 upload

---
+++ BAEKJOON/cpp/11549_Identifying_tea.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, answer = 0;
+vector<int> v;
+
+void input() {
+	cin >> n;
+
+	int a;
+	for (int i = 0; i < 5; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < 5; i++) {
+		if (v[i] == n) {
+			answer++;
+		}
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/2278_Percentage_of_Letter_in_String.cpp
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+	int percentageLetter(string s, char letter) {
+		int cnt = 0, answer = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == letter)
+				cnt++;
+		}
+
+		answer = ((double)cnt / s.size()) * 100;
+
+		return answer;
+	}
+};
