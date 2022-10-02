f15e0b939e0d0a62a581edea2d0f1f844734798d
hyee0715 <hyee0715@naver.com>
2022-10-02 11:33:33 +0900

    221002 upload

---
+++ BAEKJOON/cpp/10569_다면체.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int v, e;
+
+void input() {
+	cin >> v >> e;
+}
+
+void solution() {
+	int n = 2 - v + e;
+
+	cout << n << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/1380_Lucky_Numbers_in_a_Matrix.cpp
@@ -0,0 +1,33 @@
+﻿class Solution {
+public:
+	bool check(vector<vector<int>>& matrix, int num, int a, int b) {
+		for (int i = 0; i < matrix.size(); i++) {
+			if (matrix[i][b] > num) {
+				return false;
+			}
+		}
+
+		for (int i = 0; i < matrix[0].size(); i++) {
+			if (matrix[a][i] < num) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+		//행의 최소 요소, 열의 최대 요소
+		vector<int> answer;
+
+		for (int i = 0; i < matrix.size(); i++) {
+			for (int j = 0; j < matrix[i].size(); j++) {
+				if (check(matrix, matrix[i][j], i, j)) {
+					answer.push_back(matrix[i][j]);
+				}
+			}
+		}
+
+		return answer;
+	}
+};
