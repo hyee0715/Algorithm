1d7a26d021c0b552a1bb84ef51f97c8b18247642
hyee0715 <hyee0715@naver.com>
2022-10-03 23:34:18 +0900

    221003 upload

---
+++ BAEKJOON/cpp/2028_자기복제수.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+
+void input() {
+	cin >> n;
+}
+
+void solution() {
+	int square = n * n;
+	string st_squre = to_string(square);
+	string st_n = to_string(n);
+
+	int idx = st_squre.size() - 1;
+
+	for (int i = st_n.size() - 1; i >= 0; i--) {
+		if (st_n[i] != st_squre[idx]) {
+			cout << "NO\n";
+			return;
+		}
+		
+		idx--;
+	}
+	
+	cout << "YES\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/1935_Maximum_Number_of_Words_You_Can_Type.cpp
@@ -0,0 +1,43 @@
+﻿class Solution {
+public:
+	int canBeTypedWords(string text, string brokenLetters) {
+		unordered_map<char, int> um;
+		vector<string> v;
+		string s;
+		int answer = 0;
+
+		for (int i = 0; i < brokenLetters.size(); i++) {
+			um[brokenLetters[i]]++;
+		}
+
+		for (int i = 0; i < text.size(); i++) {
+			if (text[i] == ' ') {
+				v.push_back(s);
+				s.clear();
+			}
+			else {
+				s += text[i];
+			}
+		}
+
+		v.push_back(s);
+		s.clear();
+
+		for (int i = 0; i < v.size(); i++) {
+			bool flag = false;
+
+			for (int j = 0; j < v[i].size(); j++) {
+				if (um[v[i][j]] != 0) {
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag) {
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+};
