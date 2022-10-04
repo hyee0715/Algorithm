58dd86bf9e12374fe7e2c700b59af418a539e0f0
hyee0715 <hyee0715@naver.com>
2022-10-04 18:37:37 +0900

    221004 upload

---
+++ BAEKJOON/cpp/13163_닉네임에_갓_붙이기.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> v;
+string s, answer;
+
+void input() {
+	v.clear();
+	s.clear();
+	answer.clear();
+	cin.ignore();
+
+	getline(cin, s);
+}
+
+void solution() {
+	string temp;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == ' ') {
+			v.push_back(temp);
+			temp.clear();
+		}
+		else {
+			temp += s[i];
+		}
+	}
+
+	if (!temp.empty()) {
+		v.push_back(temp);
+		temp.clear();
+	}
+
+	answer += "god";
+
+	for (int i = 1; i < v.size(); i++) {
+		answer += v[i];
+	}
+
+	cout << answer << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		input();
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/2422_Merge_Operations_to_Turn_Array_Into_a_Palindrome.cpp
@@ -0,0 +1,34 @@
+﻿class Solution {
+public:
+	int minimumOperations(vector<int>& nums) {
+		int answer = 0;
+
+		int left = 0;
+		int right = nums.size() - 1;
+
+		long long right_sum = nums[right];
+		long long left_sum = nums[left];
+
+		while (left < right) {
+			if (left_sum > right_sum) {
+				answer++;
+				right--;
+				right_sum += nums[right];
+			}
+			else if (left_sum < right_sum) {
+				answer++;
+				left++;
+				left_sum += nums[left];
+			}
+			else {
+				left++;
+				right--;
+
+				left_sum = nums[left];
+				right_sum = nums[right];
+			}
+		}
+
+		return answer;
+	}
+};
