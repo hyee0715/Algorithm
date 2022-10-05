e878226254d4ec8db4704fac2aa37f9eec0e28d4
hyee0715 <hyee0715@naver.com>
2022-10-05 15:02:23 +0900

    221005 upload

---
+++ BAEKJOON/cpp/2145_숫자_놀이.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+
+void solution() {
+	int sum = 0;
+
+	while (n >= 10) {
+		while (n > 0) {
+			sum += (n % 10);
+			n = n / 10;
+		}
+
+		n = sum;
+		sum = 0;
+	}
+
+	cout << n << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		cin >> n;
+
+		if (n == 0) break;
+
+		solution();
+	}
+
+	return 0;
+}
---
+++ LeetCode/cpp/922_Sort_Array_By_Parity_II.cpp
@@ -0,0 +1,28 @@
+﻿class Solution {
+public:
+	vector<int> sortArrayByParityII(vector<int>& nums) {
+		vector<int> even, odd, answer;
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (nums[i] % 2 == 0) {
+				even.push_back(nums[i]);
+			}
+			else {
+				odd.push_back(nums[i]);
+			}
+		}
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (i % 2 == 0) {
+				answer.push_back(even[even.size() - 1]);
+				even.pop_back();
+			}
+			else {
+				answer.push_back(odd[odd.size() - 1]);
+				odd.pop_back();
+			}
+		}
+
+		return answer;
+	}
+};
