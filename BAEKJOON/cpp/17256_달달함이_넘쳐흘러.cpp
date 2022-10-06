b9ae2c6fca3aef07ee2290c5cb7db81ab9c7381d
hyee0715 <hyee0715@naver.com>
2022-10-06 17:53:47 +0900

    221006 upload

---
+++ BAEKJOON/cpp/17256_달달함이_넘쳐흘러.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+pair<pair<int, int>, int> a, b, c;
+
+void input() {
+	cin >> a.first.first >> a.first.second >> a.second 
+		>> c.first.first >> c.first.second >> c.second;
+}
+
+void solution() {
+	b.first.first = abs(a.second - c.first.first);
+	b.first.second = (double)c.first.second / a.first.second;
+	b.second = abs(a.first.first - c.second);
+
+	cout << b.first.first << " " << b.first.second << " " << b.second;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/700_Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	void search(TreeNode* root, int val, TreeNode*& answer) {
+		if (root == nullptr)
+			return;
+
+		if (root->val == val) {
+			answer = root;
+			return;
+		}
+
+		search(root->left, val, answer);
+		search(root->right, val, answer);
+	}
+
+	TreeNode* searchBST(TreeNode* root, int val) {
+		TreeNode* answer = nullptr;
+		search(root, val, answer);
+
+		return answer;
+	}
+};
