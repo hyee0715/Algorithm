3c7f7b4926146cdd323936d80db941c49299a7b6
hyee0715 <hyee0715@naver.com>
2022-10-12 22:51:42 +0900

    221012 upload

---
+++ BAEKJOON/cpp/25640_MBTI.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int n, answer = 0;
+string jinho;
+vector<string> v;
+
+void input() {
+	cin >> jinho;
+	cin >> n;
+
+	string s;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+
+		v.push_back(s);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < v.size(); i++) {
+		if (jinho == v[i])
+			answer++;
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/925_Long_Pressed_Name.cpp
@@ -0,0 +1,32 @@
+﻿class Solution {
+public:
+	bool isLongPressedName(string name, string typed) {
+		char prev = name[0];
+		int p1 = 0, p2 = 0;
+
+		if (name.size() > typed.size())
+			return false;
+
+		while (p2 < typed.size()) {
+			if (name[p1] == typed[p2]) {
+				p2++;
+
+				prev = name[p1];
+				p1++;
+			}
+			else {
+				if (prev == typed[p2]) {
+					p2++;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+
+		if (p1 < name.size())
+			return false;
+
+		return true;
+	}
+};
