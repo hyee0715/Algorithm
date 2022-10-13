3e50a1469883e954df23999ada91e11cbf105519
hyee0715 <hyee0715@naver.com>
2022-10-13 22:50:16 +0900

    221013 upload

---
+++ BAEKJOON/cpp/19944_뉴비의_기준은_뭘까.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+
+void input() {
+	cin >> n >> m;
+}
+
+void solution() {
+	if (m == 1 || m == 2) {
+		cout << "NEWBIE!";
+	}
+	else if (m <= n) {
+		cout << "OLDBIE!";
+	}
+	else {
+		cout << "TLE!";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
