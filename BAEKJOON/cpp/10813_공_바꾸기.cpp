878723bc58ab8623ed1fd1297784d4906a153b85
hyee0715 <hyee0715@naver.com>
2022-10-15 21:43:56 +0900

    221015 upload

---
+++ BAEKJOON/cpp/10813_공_바꾸기.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+void input() {
+	cin >> n >> m;
+
+	v.push_back(-1);
+
+	for (int i = 1; i <= n; i++) {
+		v.push_back(i);
+	}
+}
+
+void solution() {
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+
+		int temp = v[a];
+		v[a] = v[b];
+		v[b] = temp;
+	}
+
+	for (int i = 1; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1295_Find_Numbers_with_Even_Number_of_Digits.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+	int findNumbers(vector<int>& nums) {
+		int answer = 0;
+
+		for (int i = 0; i < nums.size(); i++) {
+			string s = to_string(nums[i]);
+			int cnt = s.size();
+
+			if (cnt % 2 == 0)
+				answer++;
+		}
+
+		return answer;
+	}
+};
