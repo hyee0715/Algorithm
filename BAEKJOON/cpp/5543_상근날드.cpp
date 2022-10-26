73b07b47b2ce5de8ff5ec40670cb835da823f447
hyee0715 <hyee0715@naver.com>
2022-10-26 21:46:30 +0900

    221026 upload

---
+++ BAEKJOON/cpp/5543_상근날드.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int answer = 4001;
+vector<int> hamburger, drink;
+
+void input() {
+	int n;
+	for (int i = 0; i < 3; i++) {
+		cin >> n;
+		hamburger.push_back(n);
+	}
+
+	for (int i = 0; i < 2; i++) {
+		cin >> n;
+		drink.push_back(n);
+	}
+}
+
+void solution() {
+	for (int i = 0; i < hamburger.size(); i++) {
+		for (int j = 0; j < drink.size(); j++) {
+			if (answer > hamburger[i] + drink[j] - 50) {
+				answer = hamburger[i] + drink[j] - 50;
+			}
+		}
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1122_Relative_Sort_Array.cpp
@@ -0,0 +1,30 @@
+﻿class Solution {
+public:
+	vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+		map<int, int> mp;
+		vector<int> answer;
+
+		for (int i = 0; i < arr1.size(); i++) {
+			mp[arr1[i]]++;
+		}
+
+		for (int i = 0; i < arr2.size(); i++) {
+			while (mp[arr2[i]] != 0) {
+				answer.push_back(arr2[i]);
+				mp[arr2[i]]--;
+			}
+		}
+
+		for (auto m : mp) {
+			if (m.second != 0) {
+				while (mp[m.first] != 0) {
+					3
+						answer.push_back(m.first);
+					mp[m.first]--;
+				}
+			}
+		}
+
+		return answer;
+	}
+};
