76d267c5afc754d09a187fe5566179fdc3f39c97
hyee0715 <hyee0715@naver.com>
2022-11-02 15:07:14 +0900

    221102 upload

---
+++ BAEKJOON/cpp/25304_영수증.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int x, n, a, b, sum = 0;
+
+void solution() {
+	cin >> x >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+
+		sum += (a * b);
+	}
+
+	if (sum == x)
+		cout << "Yes";
+	else
+		cout << "No";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solution();
+
+	return 0;
+}
