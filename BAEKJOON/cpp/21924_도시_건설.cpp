844913168935309871c4df64a3e4a99486c24a94
hyee0715 <hyee0715@naver.com>
2022-11-03 23:52:18 +0900

    221103 upload

---
+++ BAEKJOON/cpp/21924_도시_건설.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+long long all_path_sum = 0, minimum_cost = 0;
+vector<pair<int, pair<int, int>>> paths;
+vector<int> parent;
+
+void input() {
+	cin >> n >> m;
+
+	int a, b, c;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+		paths.push_back({ c, {a, b} });
+
+		all_path_sum += c;
+	}
+}
+
+void parent_initialize() {
+	parent.assign(n + 1, 0);
+
+	for (int i = 1; i <= n; i++) {
+		parent[i] = i;
+	}
+}
+
+int find_parent(int x) {
+	if (parent[x] == x) return x;
+	else return parent[x] = find_parent(parent[x]);
+}
+
+void union_parent(int x, int y) {
+	x = find_parent(x);
+	y = find_parent(y);
+
+	if (x < y)
+		parent[y] = x;
+	else
+		parent[x] = y;
+}
+
+bool same_parent(int x, int y) {
+	x = find_parent(x);
+	y = find_parent(y);
+
+	if (x == y)
+		return true;
+	return false;
+}
+
+bool are_paths_all_connected() {
+	for (int i = 1; i < n; i++) {
+		if (same_parent(i, i + 1) == false)
+			return false;
+	}
+
+	return true;
+}
+
+void solution() {
+	parent_initialize();
+
+	sort(paths.begin(), paths.end());
+
+	for (int i = 0; i < paths.size(); i++) {
+		if (same_parent(paths[i].second.first, paths[i].second.second) == false) {
+			union_parent(paths[i].second.first, paths[i].second.second);
+
+			minimum_cost += paths[i].first;
+		}
+	}
+
+	if (!are_paths_all_connected()) {
+		cout << -1;
+		return;
+	}
+
+	cout << all_path_sum - minimum_cost;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ LeetCode/cpp/1422_Maximum_Score_After_Splitting_a_String.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+	int maxScore(string s) {
+		int answer = -1;
+
+		int cnt = 1;
+		for (int i = 0; i < s.size() - 1; i++) {
+			int left = 0, right = 0;
+
+			for (int j = 0; j < cnt; j++) {
+				if (s[j] == '0') {
+					left++;
+				}
+			}
+
+			for (int j = cnt; j < s.size(); j++) {
+				if (s[j] == '1') {
+					right++;
+				}
+			}
+
+			cnt++;
+			int score = left + right;
+			answer = max(answer, score);
+		}
+
+		return answer;
+	}
+};
