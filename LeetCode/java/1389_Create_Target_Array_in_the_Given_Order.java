3ca7e6f8a370c1be6854d650fb61cbe90bfdd758
hyee0715 <hyee0715@naver.com>
2023-01-25 18:46:51 +0900

    230125 upload

---
+++ LeetCode/java/1389_Create_Target_Array_in_the_Given_Order.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
