d0a82a5bcc751c9d00b62cf2b556e9919de1994c
hyee0715 <hyee0715@naver.com>
2023-02-01 15:16:19 +0900

    230201 upload

---
+++ LeetCode/cpp/2139_Minimum_Moves_to_Reach_Target_Score.cpp
@@ -0,0 +1,36 @@
+﻿class Solution {
+public:
+	int minMoves(int target, int maxDoubles) {
+		if (maxDoubles == 0) {
+			return target - 1;
+		}
+
+		int answer = 0;
+		bool stop = false;
+
+		while (target > 1) {
+			while (target % 2 == 0 && maxDoubles > 0) {
+				target /= 2;
+				maxDoubles--;
+				answer++;
+
+				if (target == 1) {
+					return answer;
+				}
+			}
+
+			if (maxDoubles == 0) {
+				return answer + target - 1;
+			}
+
+			target--;
+			answer++;
+
+			if (target == 1) {
+				break;
+			}
+		}
+
+		return answer;
+	}
+};
---
+++ LeetCode/java/2139_Minimum_Moves_to_Reach_Target_Score.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        if (maxDoubles == 0) {
+            return target - 1;
+        }
+
+        int answer = 0;
+        boolean stop = false;
+
+        while (target > 1) {
+            while (target % 2 == 0 && maxDoubles > 0) {
+                target /= 2;
+                maxDoubles--;
+                answer++;
+
+                if (target == 1) {
+                    return answer;
+                }
+            }
+
+            if (maxDoubles == 0) {
+                return answer + target - 1;
+            }
+
+            target--;
+            answer++;
+
+            if (target == 1) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
