49f4c6c8175794287b81d0b7a83ede9da6106bed
hyee0715 <hyee0715@naver.com>
2023-02-26 17:42:53 +0900

    230226 upload

---
+++ LeetCode/cpp/633_Sum_of_Square_Numbers.cpp
@@ -0,0 +1,14 @@
+﻿class Solution {
+public:
+	bool judgeSquareSum(int c) {
+		for (long long i = 0; i * i <= c; i++) {
+			double b = sqrt(c - (i * i));
+
+			if (b == (int)b) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+};
