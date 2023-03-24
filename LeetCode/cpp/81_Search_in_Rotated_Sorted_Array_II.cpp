0697409c955dfa198bddb48f65dac1ccba68b705
hyee0715 <hyee0715@naver.com>
2023-03-24 19:11:47 +0900

    230324 upload

---
+++ LeetCode/cpp/81_Search_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,48 @@
+﻿class Solution {
+public:
+	bool search(vector<int>& nums, int target) {
+		if (nums.size() == 1) {
+			if (nums[0] == target) {
+				return true;
+			}
+			return false;
+		}
+
+		int left = 0, right = nums.size() - 1;
+
+		while (left <= right) {
+			while (left < right && nums[left] == nums[left + 1]) {
+				left++;
+			}
+
+			while (left < right && nums[right - 1] == nums[right]) {
+				right--;
+			}
+
+			int mid = left + (right - left) / 2;
+
+			if (nums[mid] == target) {
+				return true;
+			}
+
+			if (nums[left] <= nums[mid]) {
+				if (nums[left] <= target && target < nums[mid]) {
+					right = mid - 1;
+				}
+				else {
+					left = mid + 1;
+				}
+			}
+			else {
+				if (nums[mid] < target && target <= nums[right]) {
+					left = mid + 1;
+				}
+				else {
+					right = mid - 1;
+				}
+			}
+		}
+
+		return false;
+	}
+};
