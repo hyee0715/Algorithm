59d810e4032a88b9f695d8ed0d5e25027e7d9cc5
hyee0715 <hyee0715@naver.com>
2023-05-03 20:26:52 +0900

    230503 upload

---
+++ LeetCode/cpp/256_Paint_House.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	int minCost(vector<vector<int>>& costs) {
+		vector<vector<int>> dp(costs.size(), vector<int>(3, 0));
+
+		dp[0][0] = costs[0][0];
+		dp[0][1] = costs[0][1];
+		dp[0][2] = costs[0][2];
+
+		for (int i = 1; i < costs.size(); i++) {
+			dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+			dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+			dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+		}
+
+		int answer = min(min(dp[costs.size() - 1][0], dp[costs.size() - 1][1]), dp[costs.size() - 1][2]);
+
+		return answer;
+	}
+};
