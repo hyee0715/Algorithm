c6537222078bef671e98e1c7164b6173348dff12
hyee0715 <hyee0715@naver.com>
2023-05-05 18:01:59 +0900

    230505 upload

---
+++ LeetCode/java/2600_K_Items_With_the_Maximum_Sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int answer = 0;
+
+        if (numOnes <= k) {
+            answer += numOnes;
+            k -= numOnes;
+        } else {
+            answer += k;
+            k = 0;
+        }
+
+        if (k == 0) {
+            return answer;
+        }
+
+        if (numZeros <= k) {
+            k -= numZeros;
+        } else {
+            k = 0;
+        }
+
+        if (numNegOnes <= k) {
+            answer -= numNegOnes;
+            k -= numNegOnes;
+        } else {
+            answer -= k;
+        }
+
+        return answer;
+    }
+}
