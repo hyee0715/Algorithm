b858596414e8d4517ba01b8ba38cddfd158fe9fd
hyee0715 <hyee0715@naver.com>
2023-05-18 23:40:54 +0900

    230518 upload

---
+++ SW_Expert_Academy/cpp/D3_1225_암호생성기.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int tc;
+queue<int> q;
+
+void input() {
+	cin >> tc;
+	
+	for (int i = 0; i < 8; i++) {
+		int a;
+		cin >> a;
+		q.push(a);
+	}
+}
+
+void solution() {
+	int cnt = 1;
+
+	while (1) {
+		int n = q.front();
+		q.pop();
+
+		n -= cnt;
+
+		if (n <= 0) {
+			n = 0;
+			q.push(n);
+
+			break;
+		}
+
+		q.push(n);
+
+		if (cnt == 5) {
+			cnt = 1;
+		}
+		else {
+			cnt++;
+		}
+	}
+
+	while (!q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 1; i <= 10; i++) {
+		input();
+
+		cout << "#" << i << " ";
+		solution();
+		cout << "\n";
+	}
+
+	return 0;
+}
---
+++ SW_Expert_Academy/cpp/D3_2805_농작물_수확하기.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int n;
+vector<vector<int>> board;
+
+void initialize() {
+	board.clear();
+}
+
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+
+		vector<int> v;
+		for (int j = 0; j < n; j++) {
+			v.push_back(s[j] - '0');
+		}
+
+		board.push_back(v);
+	}
+}
+
+void solution() {
+	int answer = 0;
+	int cnt = 0;
+
+	bool up = true;
+
+	for (int i = 0; i < board.size(); i++) {
+		answer += board[i][n / 2];
+
+		for (int j = 1; j <= cnt; j++) {
+			answer += board[i][(n / 2) + j];
+			answer += board[i][(n / 2) - j];
+		}
+
+		if (cnt == n / 2) {
+			up = false;
+		}
+
+		if (up) {
+			cnt++;
+		}
+		else {
+			cnt--;
+		}
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+
+	for (int i = 1; i <= tc; i++) {
+		initialize();
+		input();
+
+		cout << "#" << i << " ";
+		solution();
+		cout << "\n";
+	}
+
+	return 0;
+}
