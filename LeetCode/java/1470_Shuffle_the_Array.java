69c51b37097500a320911f0c57f30f80a1a90ad9
hyee0715 <hyee0715@naver.com>
2023-05-21 22:20:45 +0900

    230521 upload

---
+++ LeetCode/java/1470_Shuffle_the_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[nums.length];
+
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer[idx++] = nums[i];
+            answer[idx++] = nums[i + n];
+        }
+
+        return answer;
+    }
+}
