ddf536553a03ebbc3d8cedf23f22e313e5df8882
hyee0715 <hyee0715@naver.com>
2023-05-30 15:00:40 +0900

    230530 upload

---
+++ LeetCode/cpp/1925_Count_Square_Sum_Triples.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+	int countTriples(int n) {
+		int answer = 0;
+
+		for (int a = 1; a <= n; a++) {
+			for (int b = 1; b <= n; b++) {
+				for (int c = 1; c <= n; c++) {
+					if (a * a + b * b == c * c) {
+						answer++;
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+};
