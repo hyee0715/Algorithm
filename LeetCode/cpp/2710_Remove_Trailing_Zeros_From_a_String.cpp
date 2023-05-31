445d868e472889ed23d108764b5b25545f607531
hyee0715 <hyee0715@naver.com>
2023-05-31 19:34:37 +0900

    230531 upload

---
+++ LeetCode/cpp/2710_Remove_Trailing_Zeros_From_a_String.cpp
@@ -0,0 +1,15 @@
+﻿class Solution {
+public:
+	string removeTrailingZeros(string num) {
+		for (int i = num.length() - 1; i >= 0; i--) {
+			if (num[i] == '0') {
+				num.erase(num.begin() + i);
+			}
+			else {
+				break;
+			}
+		}
+
+		return num;
+	}
+};
