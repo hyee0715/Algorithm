a94a5568ac4ec91676566877a203a7426c149193
hyee0715 <hyee0715@naver.com>
2023-06-10 22:12:23 +0900

    230610 upload

---
+++ LeetCode/cpp/706_Design_HashMap.cpp
@@ -0,0 +1,34 @@
+﻿class MyHashMap {
+public:
+	unordered_map<int, int> um;
+
+	MyHashMap() {
+
+	}
+
+	void put(int key, int value) {
+		um[key] = value;
+	}
+
+	int get(int key) {
+		if (um.count(key)) {
+			return um[key];
+		}
+
+		return -1;
+	}
+
+	void remove(int key) {
+		if (um.count(key)) {
+			um.erase(key);
+		}
+	}
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
