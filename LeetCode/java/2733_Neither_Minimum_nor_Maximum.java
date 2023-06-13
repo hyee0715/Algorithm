f9738b8c66c05cf36c316aa9a1e59860ad31c4d4
hyee0715 <hyee0715@naver.com>
2023-06-13 19:06:19 +0900

    230613 upload

---
+++ LeetCode/java/2733_Neither_Minimum_nor_Maximum.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if (nums.length <= 2) {
+            return -1;
+        }
+
+        Arrays.sort(nums);
+
+        return nums[1];
+    }
+}
