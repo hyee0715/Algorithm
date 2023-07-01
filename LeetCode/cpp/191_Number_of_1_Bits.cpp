d579aa6d6dfc03a6970dc7079a8535d60bcc3bf5
hyee0715 <hyee0715@naver.com>
2023-07-01 20:00:53 +0900

    230701 upload

---
+++ LeetCode/cpp/191_Number_of_1_Bits.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	int hammingWeight(uint32_t n) {
+		int answer = 0;
+		long long num = n;
+
+		while (num > 0) {
+			if (num % 2 != 0) {
+				answer++;
+			}
+
+			num /= 2;
+		}
+
+		return answer;
+	}
+};
