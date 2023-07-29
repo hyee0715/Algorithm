eab9a9186363ded61be1c6c766acc98d430fa1d2
hyee0715 <hyee0715@naver.com>
2023-07-29 23:30:16 +0900

    230729 upload

---
+++ LeetCode/cpp/1221_Split_a_String_in_Balanced_Strings.cpp
@@ -0,0 +1,25 @@
+﻿class Solution {
+public:
+	int balancedStringSplit(string s) {
+		int answer = 0;
+
+		int r = 0, l = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == 'R') {
+				r++;
+			}
+			else {
+				l++;
+			}
+
+			if (r == l) {
+				answer++;
+				r = 0;
+				l = 0;
+			}
+		}
+
+		return answer;
+	}
+};
