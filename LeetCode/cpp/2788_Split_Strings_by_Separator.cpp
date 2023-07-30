9470f8d5fc9ee6bfdaeceb2bd71243a7784919b1
hyee0715 <hyee0715@naver.com>
2023-07-30 23:59:36 +0900

    230730 upload

---
+++ LeetCode/cpp/2788_Split_Strings_by_Separator.cpp
@@ -0,0 +1,28 @@
+﻿class Solution {
+public:
+	vector<string> splitWordsBySeparator(vector<string>& words, char separator) {
+		vector<string> answer;
+
+		string s;
+		for (int i = 0; i < words.size(); i++) {
+			for (int j = 0; j < words[i].length(); j++) {
+				if (words[i][j] == separator) {
+					if (!s.empty()) {
+						answer.push_back(s);
+						s.clear();
+					}
+				}
+				else {
+					s += words[i][j];
+				}
+			}
+
+			if (!s.empty()) {
+				answer.push_back(s);
+				s.clear();
+			}
+		}
+
+		return answer;
+	}
+};
