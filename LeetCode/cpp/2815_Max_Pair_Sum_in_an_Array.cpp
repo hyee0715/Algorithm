c6195b6cdb960910140dc053ff53c7c25f584fdc
hyee0715 <hyee0715@naver.com>
2023-09-02 23:59:34 +0900

    230902 upload

---
+++ LeetCode/cpp/2815_Max_Pair_Sum_in_an_Array.cpp
@@ -0,0 +1,35 @@
+﻿class Solution {
+public:
+	int maxSum(vector<int>& nums) {
+		int answer = -1;
+
+		unordered_map<int, vector<int>> um;
+
+		for (int n : nums) {
+			string s = to_string(n);
+
+			int maxDigit = 0;
+			for (int i = 0; i < s.length(); i++) {
+				if (maxDigit < (s[i] - '0')) {
+					maxDigit = s[i] - '0';
+				}
+			}
+
+			um[maxDigit].push_back(n);
+		}
+
+		for (auto m : um) {
+			if (m.second.size() == 1) {
+				continue;
+			}
+
+			vector<int> v = m.second;
+
+			sort(v.begin(), v.end());
+
+			answer = max(answer, v[v.size() - 1] + v[v.size() - 2]);
+		}
+
+		return answer;
+	}
+};
