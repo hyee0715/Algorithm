fdee6d447240f1a7c6ec0c33c8954a5c2d2d1eaa
hyee0715 <hyee0715@naver.com>
2023-09-23 19:33:31 +0900

    230923 upload

---
+++ Programmers/cpp/LEVEL_2_롤케이크_자르기.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+	int answer = 0;
+
+	unordered_map<int, int> um1, um2;
+
+	for (int n : topping) {
+		um2[n]++;
+	}
+
+	for (int n : topping) {
+		um1[n]++;
+		um2[n]--;
+
+		if (um2[n] == 0) {
+			um2.erase(n);
+		}
+
+		if (um1.size() == um2.size()) {
+			answer++;
+		}
+	}
+
+	return answer;
+}
