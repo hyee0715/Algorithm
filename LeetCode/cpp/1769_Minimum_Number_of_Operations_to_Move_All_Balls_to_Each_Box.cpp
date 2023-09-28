6414d195593029b9ee1e309e84369770df60973d
hyee0715 <hyee0715@naver.com>
2023-09-28 21:55:21 +0900

    230928 upload

---
+++ LeetCode/cpp/1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.cpp
@@ -0,0 +1,16 @@
+﻿class Solution {
+public:
+	vector<int> minOperations(string boxes) {
+		vector<int> answer(boxes.length(), 0);
+
+		for (int i = 0; i < answer.size(); i++) {
+			for (int j = 0; j < boxes.length(); j++) {
+				if (boxes[j] == '1') {
+					answer[i] += abs(i - j);
+				}
+			}
+		}
+
+		return answer;
+	}
+};
