18aebd9e349d882113c7ea7aff05610bd7e4ae92
hyee0715 <hyee0715@naver.com>
2023-10-19 23:52:33 +0900

    231019 upload

---
+++ LeetCode/java/1551_Minimum_Operations_to_Make_Array_Equal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(int n) {
+        int answer = 0;
+
+        int num = 0;
+        if (n % 2 == 0) {
+            num = 2 * (n / 2);
+        } else {
+            num = (2 * (n / 2)) + 1;
+        }
+
+        for (int i = 1; i < n; i += 2) {
+            answer += (num - i);
+        }
+
+        return answer;
+    }
+}
