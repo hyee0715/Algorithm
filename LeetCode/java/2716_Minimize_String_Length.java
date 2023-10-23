597bc90391abe2db1e6da9e76ddc32f2f21bd00e
hyee0715 <hyee0715@naver.com>
2023-10-23 23:48:17 +0900

    231023 upload

---
+++ LeetCode/java/2716_Minimize_String_Length.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int minimizedStringLength(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+
+        return set.size();
+    }
+}
