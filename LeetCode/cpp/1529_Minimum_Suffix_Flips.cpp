4b9d0e999c111019e4afae168481ddb0a4c705e6
hyee0715 <hyee0715@naver.com>
2023-10-25 22:45:34 +0900

    231025 upload

---
+++ LeetCode/cpp/1529_Minimum_Suffix_Flips.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+	int minFlips(string target) {
+		int answer = 0;
+
+		char current = '0';
+
+		for (int i = 0; i < target.length(); i++) {
+			if (target[i] != current) {
+				if (current == '0') {
+					current = '1';
+				}
+				else {
+					current = '0';
+				}
+
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+};
---
+++ LeetCode/java/1529_Minimum_Suffix_Flips.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minFlips(String target) {
+        int answer = 0;
+
+        char current = '0';
+
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) != current) {
+                if (current == '0') {
+                    current = '1';
+                } else {
+                    current = '0';
+                }
+
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
