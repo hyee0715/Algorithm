3b4adcaa41af94ce78803fd6642fb0a4c7db5e39
hyee0715 <hyee0715@naver.com>
2024-01-29 23:09:12 +0900

    240129 upload

---
+++ Programmers/cpp/LEVEL_3_거스름돈.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> money) {
+	int answer = 0;
+
+	vector<int> dp(n + 1, 0);
+
+	dp[0] = 1;
+
+	for (int m : money) {
+		for (int i = m; i <= n; i++) {
+			dp[i] = dp[i - m] + dp[i];
+		}
+	}
+
+	answer = dp[n];
+
+	return answer;
+}
