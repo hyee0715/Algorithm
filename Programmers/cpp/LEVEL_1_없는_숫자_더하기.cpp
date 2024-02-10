ae02bc2e062bc5a968afccebe9953a0d982069d1
hyee0715 <hyee0715@naver.com>
2024-02-10 20:22:15 +0900

    240210 upload

---
+++ Programmers/cpp/LEVEL_1_없는_숫자_더하기.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+	int answer = 0;
+
+	unordered_set<int> st;
+
+	for (int n : numbers) {
+		st.insert(n);
+	}
+
+	for (int i = 0; i <= 9; i++) {
+		if (st.find(i) == st.end()) {
+			answer += i;
+		}
+	}
+
+	return answer;
+}
---
+++ Programmers/java/LEVEL_1_없는_숫자_더하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int n : numbers) {
+            set.add(n);
+        }
+
+        for (int i = 0; i <= 9; i++) {
+            if (!set.contains(i)) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
