0ed54fa48f97a60505532646969a2dfe4e43fbdf
hyee0715 <hyee0715@naver.com>
2024-03-29 17:32:57 +0900

    240329 upload

---
+++ BAEKJOON/cpp/14501_퇴사.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int, int>> days; //상담을 완료했을 때 걸리는 시간(days), 상담을 했을 때 받을 수 있는 금액
+int answer = 0;
+vector<int> v; //조합으로 뽑은 상담 날짜
+vector<bool> visit;
+
+void input() {
+	cin >> n;
+
+	int a, b;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		days.push_back({ a, b });
+	}
+}
+
+int validateConsultationDays() {
+	if (v.size() > 1) {
+		for (int i = 0; i < v.size() - 1; i++) {
+			int idx = v[i]; //상담 날짜
+			int nextIdx = v[i + 1];
+
+			int time = days[idx].first; //해당 날에 상담했을 경우 걸리는 시간
+
+			if (idx + time > nextIdx) {
+				return -1;
+			}
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		int idx = v[i]; //상담 날짜
+		int time = days[idx].first; //해당 날에 상담했을 경우 걸리는 시간
+
+		if (idx + time > n) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+int getConsultationFee() {
+	if (validateConsultationDays() == -1) {
+		return -1;
+	}
+
+	int sum = 0; 
+
+	for (int i = 0; i < v.size(); i++) {
+		int idx = v[i];
+
+		int fee = days[idx].second;
+
+		sum += fee;
+	}
+
+	return sum;
+}
+
+void dfs(int idx, int cnt, int limit) {
+	if (cnt == limit) {
+		if (cnt == 10) {
+			int a = 0;
+		}
+
+		int fee = getConsultationFee();
+
+		if (fee != -1) {
+			answer = max(answer, fee);
+		}
+
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (visit[i] == true) continue;
+		visit[i] = true;
+		v.push_back(i);
+		dfs(i, cnt + 1, limit);
+		visit[i] = false;
+		v.pop_back();
+	}
+}
+
+void solution() {
+	visit.assign(n, false);
+
+	for (int i = 1; i <= n; i++) {
+		//조합
+		dfs(0, 0, i);
+	}
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ BAEKJOON/cpp/17143_낚시왕.cpp
@@ -0,0 +1,212 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Shark {
+	int x;
+	int y;
+	int speed;
+	int direction;
+	int size;
+};
+
+int dx[] = { 0, 1, 0, -1 }; //동남서북
+int dy[] = { 1, 0, -1, 0 };
+
+int r, c, m;
+vector<vector<vector<Shark> > > board;
+vector<Shark> sharks;
+int anglerIdx = -1;
+int answer = 0;
+
+int convertDirection(int dir) {
+	if (dir == 1) {
+		return 3;
+	}
+	else if (dir == 2) {
+		return 1;
+	}
+	else if (dir == 3) {
+		return 0;
+	}
+	else {
+		return 2;
+	}
+}
+
+void input() {
+	cin >> r >> c >> m;
+
+	board.assign(r, vector<vector<Shark>>(c, vector<Shark>(0)));
+
+	int r, c, s, d, z;
+	for (int i = 0; i < m; i++) {
+		cin >> r >> c >> s >> d >> z;
+
+		int dir = convertDirection(d);
+
+		Shark shark = { r - 1, c - 1, s, dir, z };
+		sharks.push_back(shark);
+	}
+}
+
+void settingSharksToBoard() {
+	for (Shark shark : sharks) {
+		int x = shark.x;
+		int y = shark.y;
+
+		board[x][y].push_back(shark);
+	}
+}
+
+int findSharkIdx(Shark shark) {
+	for (int i = 0; i < sharks.size(); i++) {
+		int x = sharks[i].x;
+		int y = sharks[i].y;
+		int speed = sharks[i].speed;
+		int direction = sharks[i].direction;
+		int size = sharks[i].size;
+
+		if (shark.x == x && shark.y == y && shark.speed == speed && shark.direction == direction && shark.size == size) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+void removeShark() {
+	for (int i = 0; i < board.size(); i++) {
+		if (!board[i][anglerIdx].empty()) {
+			Shark shark = board[i][anglerIdx][0];
+
+			answer += shark.size;
+
+			int sharkIdx = findSharkIdx(shark);
+			sharks.erase(sharks.begin() + sharkIdx);
+
+			board[i][anglerIdx].clear();
+
+			break;
+		}
+	}
+}
+
+void moveSharks() {
+	for (int i = 0; i < sharks.size(); i++) {
+		int x = sharks[i].x;
+		int y = sharks[i].y;
+		int speed = sharks[i].speed;
+		int direction = sharks[i].direction;
+
+		if (speed == 0) {
+			continue;
+		}
+
+		if (direction % 2 == 0) {
+			speed = speed % ((c - 1) * 2);
+		}
+		else {
+			speed = speed % ((r - 1) * 2);
+		}
+
+		int nDir = direction, nx = x, ny = y;
+
+		for (int i = 0; i < speed; i++) {
+			nDir = direction;
+			nx = x + dx[direction];
+			ny = y + dy[direction];
+
+			if (nx < 0 || ny < 0 || nx >= board.size() || ny >= board[0].size()) {
+				nDir = (direction + 2) % 4;
+				nx = x + dx[nDir];
+				ny = y + dy[nDir];
+			}
+
+			x = nx;
+			y = ny;
+			direction = nDir;
+		}
+
+		sharks[i].x = nx;
+		sharks[i].y = ny;
+		sharks[i].direction = nDir;
+	}
+}
+
+bool sharkCompare(Shark a, Shark b) {
+	if (a.size < b.size) {
+		return false;
+	}
+
+	return true;
+}
+
+void removeDuplicatedShark() {
+	//한 칸에 여러 상어가 있는 경우 크기가 가장 큰 상어 제외한 나머지 상어 모두 제거
+	sharks.clear();
+
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
+			if (board[i][j].size() > 1) {
+				vector<Shark> v = board[i][j];
+
+				sort(v.begin(), v.end(), sharkCompare);
+
+				Shark shark = v[0];
+
+				board[i][j].clear();
+				board[i][j].push_back(shark);
+				sharks.push_back(shark);
+
+			}
+			else if (board[i][j].size() == 1) {
+				sharks.push_back(board[i][j][0]);
+			}
+		}
+	}
+}
+
+void solution() {
+	while (1) {
+		if (sharks.empty()) {
+			cout << answer;
+			break;
+		}
+
+		settingSharksToBoard();
+
+		//낚시왕 오른쪽 한 칸 이동
+		anglerIdx++;
+		if (anglerIdx == c) {
+			cout << answer;
+			break;
+		}
+
+		//낚시왕이 위치한 열의 상어 중 땅과 가장 가까운 상어 잡기
+		removeShark();
+
+		//상어 이동시키기
+		moveSharks();
+
+		board.clear();
+		board.assign(r, vector<vector<Shark>>(c, vector<Shark>(0)));
+
+		settingSharksToBoard();
+
+		//한 칸에 여러 상어가 있는 경우 크기가 가장 큰 상어 제외한 나머지 상어 모두 제거
+		removeDuplicatedShark();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
