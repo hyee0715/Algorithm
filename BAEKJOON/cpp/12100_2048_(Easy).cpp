37dae03467d1bdac296737e7338f96be4b53f06a
hyee0715 <hyee0715@naver.com>
2024-03-31 21:13:01 +0900

    240331 upload

---
+++ BAEKJOON/cpp/12100_2048_(Easy).cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n;
+vector<vector<int> > board, original;
+vector<int> v;
+queue<int> q;
+int answer = 0;
+
+void input() {
+	cin >> n;
+
+	original.assign(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> original[i][j];
+		}
+	}
+}
+
+void mergeBlocks(int& num, int block) {
+	if (num == -1) {
+		num = block;
+	}
+	else {
+		if (num == block) {
+			q.push(num + block);
+			num = -1;
+		}
+		else {
+			q.push(num);
+			num = block;
+		}
+	}
+}
+
+void arrangeBlocks() {
+	int num = -1;
+
+	for (int dir : v) {
+		if (dir == 0) { //블록이 합쳐지는 방향 : 동 (왼쪽으로 스왑)
+			for (int i = 0; i < n; i++) {
+				num = -1;
+
+				for (int j = 0; j < n; j++) {
+					if (board[i][j] == 0) continue;
+
+					mergeBlocks(num, board[i][j]);
+				}
+
+				if (num != -1) {
+					q.push(num);
+				}
+
+				//큐 비우기
+				for (int j = 0; j < n; j++) {
+					board[i][j] = 0;
+				}
+
+				int idx = 0;
+				while (!q.empty()) {
+					int qNum = q.front();
+					q.pop();
+
+					board[i][idx] = qNum;
+					idx++;
+				}
+			}
+		}
+		else if (dir == 1) { //블록 합쳐지는 방향 : 북 (아래쪽으로 스왑)
+			for (int j = 0; j < n; j++) {
+				num = -1;
+
+				for (int i = n - 1; i >= 0; i--) {
+					if (board[i][j] == 0) continue;
+
+					mergeBlocks(num, board[i][j]);
+				}
+
+				if (num != -1) {
+					q.push(num);
+				}
+
+				//큐 비우기
+				for (int i = 0; i < n; i++) {
+					board[i][j] = 0;
+				}
+
+				int idx = n - 1;
+				while (!q.empty()) {
+					int qNum = q.front();
+					q.pop();
+
+					board[idx][j] = qNum;
+					idx--;
+				}
+			}
+		}
+		else if (dir == 2) { //블록 합쳐지는 방향 : 서 (오른쪽으로 스왑)
+			for (int i = 0; i < n; i++) {
+				num = -1;
+
+				for (int j = n - 1; j >= 0; j--) {
+					if (board[i][j] == 0) continue;
+
+					mergeBlocks(num, board[i][j]);
+				}
+
+				if (num != -1) {
+					q.push(num);
+				}
+
+				//큐 비우기
+				for (int j = 0; j < n; j++) {
+					board[i][j] = 0;
+				}
+
+				int idx = n - 1;
+				while (!q.empty()) {
+					int qNum = q.front();
+					q.pop();
+
+					board[i][idx] = qNum;
+					idx--;
+				}
+			}
+		}
+		else { //블록 합쳐지는 방향 : 남 (위로 스왑)
+			for (int j = 0; j < n; j++) {
+				num = -1;
+
+				for (int i = 0; i < n; i++) {
+					if (board[i][j] == 0) continue;
+
+					mergeBlocks(num, board[i][j]);
+				}
+
+				if (num != -1) {
+					q.push(num);
+				}
+
+				//큐 비우기
+				for (int i = 0; i < n; i++) {
+					board[i][j] = 0;
+				}
+
+				int idx = 0;
+				while (!q.empty()) {
+					int qNum = q.front();
+					q.pop();
+
+					board[idx][j] = qNum;
+					idx++;
+				}
+			}
+		}
+	}
+}
+
+void copyBoard() {
+	board.assign(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			board[i][j] = original[i][j];
+		}
+	}
+}
+
+int findMaxBlock() {
+	int num = -1;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			num = max(num, board[i][j]);
+		}
+	}
+
+	return num;
+}
+
+void dfs(int cnt) {
+	if (cnt == 5) {
+		copyBoard();
+
+		arrangeBlocks();
+
+		answer = max(answer, findMaxBlock());
+
+		return;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		v.push_back(i);
+		dfs(cnt + 1);
+		v.pop_back();
+	}
+}
+
+void solution() {
+	dfs(0); //중복 순열
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ BAEKJOON/cpp/14889_스타트와_링크.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<vector<int>> ability;
+vector<int> start, link;
+vector<bool> visit;
+int answer = 100000;
+
+void input() {
+	cin >> n;
+
+	ability.assign(n, vector<int>(n, 0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> ability[i][j];
+		}
+	}
+}
+
+vector<int> getLinkTeam() {
+	unordered_set<int> st;
+	vector<int> ret;
+
+	for (int i = 0; i < start.size(); i++) {
+		st.insert(start[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (st.find(i) == st.end()) {
+			ret.push_back(i);
+		}
+	}
+
+	return ret;
+}
+
+int getScore(int a, int b) {
+	int score = 0;
+
+	score += ability[a][b];
+	score += ability[b][a];
+
+	return score;
+}
+
+int getPower(bool isStartTeam) {
+	int power = 0;
+
+	if (isStartTeam) {
+		for (int i = 0; i < start.size() - 1; i++) {
+			for (int j = i + 1; j < start.size(); j++) {
+				power += getScore(start[i], start[j]);
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < link.size() - 1; i++) {
+			for (int j = i + 1; j < link.size(); j++) {
+				power += getScore(link[i], link[j]);
+			}
+		}
+	}
+
+	return power;
+}
+
+void dfs(int idx, int cnt) {
+	if (cnt == n / 2) {
+		link.clear();
+		link = getLinkTeam();
+
+		int startPower = getPower(true);
+		int linkPower = getPower(false);
+
+		int dif = abs(startPower - linkPower);
+		answer = min(answer, dif);
+
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (visit[i] == true) continue;
+		visit[i] = true;
+		start.push_back(i);
+		dfs(i, cnt + 1);
+		visit[i] = false;
+		start.pop_back();
+	}
+}
+
+void solution() {
+	visit.assign(n, false);
+
+	//조합
+	dfs(0, 0);
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
