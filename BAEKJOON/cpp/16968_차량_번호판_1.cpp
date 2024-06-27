78efe5b8636fe0838e422bdbf1d319326368b899
hyee0715 <hyee0715@naver.com>
2024-06-27 15:40:06 +0900

    240627 upload

---
+++ BAEKJOON/cpp/16968_차량_번호판_1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string s;
+int answer = 0;
+
+vector<char> list = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+vector<char> v;
+
+void input() {
+	cin >> s;
+}
+
+bool check() {
+	for (int i = 0; i < v.size() - 1; i++) {
+		if (v[i] == v[i + 1]) {
+			return false;
+		}
+	}
+
+	for (int i = 0; i < v.size(); i++) {
+		if (s[i] == 'c') {
+			if (v[i] >= '0' && v[i] <= '9') {
+				return false;
+			}
+		}
+
+		if (s[i] == 'd') {
+			if (v[i] >= 'a' && v[i] <= 'z') {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+void dfs(int cnt) {
+	if (cnt == s.length()) {
+		if (check()) {
+			answer++;
+		}
+
+		return;
+	}
+
+	for (int i = 0; i < list.size(); i++) {
+		v.push_back(list[i]);
+		dfs(cnt + 1);
+		v.pop_back();
+	}
+}
+
+void solution() {
+	//중복순열
+	dfs(0);
+
+	cout << answer;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
---
+++ BAEKJOON/cpp/16973_직사각형_탈출.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, m;
+vector<vector<int> > board;
+vector<pair<int, int> > walls;
+pair<int, int> squareSize, start, goal;
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = { 1, -1, 0, 0 };
+
+void input() {
+	cin >> n >> m;
+
+	board.assign(n, vector<int>(m, 0));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+
+			if (board[i][j] == 1) {
+				walls.push_back({ i, j });
+			}
+		}
+	}
+
+	int a, b;
+	cin >> a >> b;
+	squareSize = { a, b };
+
+	cin >> a >> b;
+	start = { a - 1, b - 1};
+
+	cin >> a >> b;
+	goal = { a - 1, b - 1};
+}
+
+bool check(int nx, int ny) {
+	if (nx < 0 || ny < 0 || nx >= n || ny >= m) return false;;
+
+	if (nx + squareSize.first - 1 >= n || ny + squareSize.second - 1 >= m) return false;;
+
+	for (int i = 0; i < walls.size(); i++) {
+		int wx = walls[i].first;
+		int wy = walls[i].second;
+
+		if (nx <= wx && nx + squareSize.first - 1 >= wx && ny <= wy && ny + squareSize.second - 1 >= wy) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int bfs(int a, int b) {
+	vector<vector<bool> > visit(n, vector<bool>(m, false));
+	queue<pair<pair<int, int>, int> > q; //x, y, cnt
+
+	q.push({ {a, b}, 0 });
+	visit[a][b] = true;
+
+	while (!q.empty()) {
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		int cnt = q.front().second;
+		q.pop();
+
+		if (x == goal.first && y == goal.second) {
+			return cnt;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (!check(nx, ny)) {
+				continue;
+			}
+
+			if (visit[nx][ny] == false) {
+				q.push({ {nx, ny}, cnt + 1 });
+				visit[nx][ny] = true;
+			}
+		}
+	}
+
+	return -1;
+}
+
+void solution() {
+	cout << bfs(start.first, start.second);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
+
+	return 0;
+}
